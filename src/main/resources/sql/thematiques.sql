90127970f2e890b72dc864596588f8aa1612f4cc
Thierry LEE <thierry.leet@gmail.com>
2023-04-06 13:32:40 +0200

    Fix Redis configuration & unwanted table creation from DTO

--- src/main/resources/sql/thematiques.sql
+++ src/main/resources/sql/thematiques.sql
@@ -1,8 +1,8 @@
-INSERT INTO "SCH_AGORA".thematiques(id, label, picto, color) VALUES (gen_random_uuid(), 'Travail & emploi', '\ud83d\udcbc', '#FFCFDEFC');
-INSERT INTO "SCH_AGORA".thematiques(id, label, picto, color) VALUES (gen_random_uuid(), 'Transition écologique', '\ud83c\udf31', '#FFCFFCD9');
-INSERT INTO "SCH_AGORA".thematiques(id, label, picto, color) VALUES (gen_random_uuid(), 'Santé', '\ud83e\ude7a', '#FFFCCFDD');
-INSERT INTO "SCH_AGORA".thematiques(id, label, picto, color) VALUES (gen_random_uuid(), 'Economie', '\ud83d\udcc8', '#FFCFF6FC');
-INSERT INTO "SCH_AGORA".thematiques(id, label, picto, color) VALUES (gen_random_uuid(), 'Education', '\ud83c\udf93', '#FFFCE7CF');
-INSERT INTO "SCH_AGORA".thematiques(id, label, picto, color) VALUES (gen_random_uuid(), 'International', '\ud83c\udf0f', '#FFE8CFFC');
-INSERT INTO "SCH_AGORA".thematiques(id, label, picto, color) VALUES (gen_random_uuid(), 'Transports', '\ud83d\ude8a', '#FFFCF7CF');
-INSERT INTO "SCH_AGORA".thematiques(id, label, picto, color) VALUES (gen_random_uuid(), 'Sécurité', '\ud83d\ude94', '#FFE1E7F3');
+INSERT INTO thematiques(label, picto, color) VALUES ('Travail & emploi', '\ud83d\udcbc', '#FFCFDEFC');
+INSERT INTO thematiques(label, picto, color) VALUES ('Transition écologique', '\ud83c\udf31', '#FFCFFCD9');
+INSERT INTO thematiques(label, picto, color) VALUES ('Santé', '\ud83e\ude7a', '#FFFCCFDD');
+INSERT INTO thematiques(label, picto, color) VALUES ('Economie', '\ud83d\udcc8', '#FFCFF6FC');
+INSERT INTO thematiques(label, picto, color) VALUES ('Education', '\ud83c\udf93', '#FFFCE7CF');
+INSERT INTO thematiques(label, picto, color) VALUES ('International', '\ud83c\udf0f', '#FFE8CFFC');
+INSERT INTO thematiques(label, picto, color) VALUES ('Transports', '\ud83d\ude8a', '#FFFCF7CF');
+INSERT INTO thematiques(label, picto, color) VALUES ('Sécurité', '\ud83d\ude94', '#FFE1E7F3');
