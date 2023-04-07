5918ad42782a5914d81ab5f4e4af542a0ca5f0cc
Thierry LEE <thierry.leet@gmail.com>
2023-04-07 10:38:55 +0200

    Add consultation details Repository

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/consultation/repository/ConsultationMapper.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/consultation/repository/ConsultationMapper.kt
@@ -1,4 +1,41 @@
 package fr.social.gouv.agora.infrastructure.consultation.repository
 
-class ConsultationMapper {
+import fr.social.gouv.agora.domain.Consultation
+import fr.social.gouv.agora.infrastructure.consultation.dto.ConsultationDTO
+import fr.social.gouv.agora.infrastructure.utils.Mapper
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class ConsultationMapper : Mapper<Consultation, ConsultationDTO> {
+
+    override fun toDomain(dto: ConsultationDTO) = Consultation(
+        id = dto.id.toString(),
+        title = dto.title,
+        abstract = dto.abstract,
+        start_date = dto.start_date,
+        end_date = dto.end_date,
+        cover = dto.cover,
+        questions_count = dto.questions_count,
+        estimated_time = dto.estimated_time,
+        participant_count_goal = dto.participant_count_goal,
+        description = dto.description,
+        tips_description = dto.tips_description,
+        id_thematique = dto.id_thematique.toString(),
+    )
+
+    override fun toDto(domain: Consultation) = ConsultationDTO(
+        id = UUID.fromString(domain.id),
+        title = domain.title,
+        abstract = domain.abstract,
+        start_date = domain.start_date,
+        end_date = domain.end_date,
+        cover = domain.cover,
+        questions_count = domain.questions_count,
+        estimated_time = domain.estimated_time,
+        participant_count_goal = domain.participant_count_goal,
+        description = domain.description,
+        tips_description = domain.tips_description,
+        id_thematique = UUID.fromString(domain.id_thematique),
+    )
 }
---
+++ src/test/kotlin/fr/social/gouv/agora/infrastructure/consultation/repository/ConsultationRepositoryImplTest.kt
@@ -0,0 +1,152 @@
+package fr.social.gouv.agora.infrastructure.consultation.repository
+
+import fr.social.gouv.agora.domain.Consultation
+import fr.social.gouv.agora.infrastructure.consultation.dto.ConsultationDTO
+import fr.social.gouv.agora.infrastructure.consultation.repository.ConsultationRepositoryImpl.Companion.CACHE_NAME
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.*
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@EnableCaching
+@ImportAutoConfiguration(
+    classes = [
+        CacheAutoConfiguration::class,
+        RedisAutoConfiguration::class,
+    ]
+)
+internal class ConsultationRepositoryImplTest {
+
+    @Autowired
+    private lateinit var repository: ConsultationRepositoryImpl
+
+    @MockBean
+    private lateinit var databaseRepository: ConsultationDatabaseRepository
+
+    @MockBean
+    private lateinit var thematiqueMapper: ConsultationMapper
+
+    @Autowired
+    @Suppress("unused")
+    private lateinit var cacheManager: CacheManager
+
+    @BeforeEach
+    fun setUp() {
+        cacheManager.getCache(CACHE_NAME)?.clear()
+    }
+
+    private val consultation = Consultation(
+        id = "domain-id",
+        title = "domain-title",
+        abstract = "domain-abstract",
+        start_date = Date(0),
+        end_date = Date(1),
+        cover = "domain-cover",
+        questions_count = "domain-questions_count",
+        estimated_time = "domain-estimated_time",
+        participant_count_goal = 6401,
+        description = "domain-description",
+        tips_description = "domain-tips_description",
+        id_thematique = "domain-id_thematique",
+    )
+
+    private val consultationDto = ConsultationDTO(
+        id = UUID.randomUUID(),
+        title = "dto-title",
+        abstract = "dto-abstract",
+        start_date = Date(0),
+        end_date = Date(1),
+        cover = "dto-cover",
+        questions_count = "dto-questions_count",
+        estimated_time = "dto-estimated_time",
+        participant_count_goal = 6401,
+        description = "dto-description",
+        tips_description = "dto-tips_description",
+        id_thematique = UUID.randomUUID(),
+    )
+
+    @Test
+    fun `getConsultation - when invalid UUID - should return null`() {
+        // When
+        val result = repository.getConsultation("lol")
+
+        // Then
+        assertThat(result).isEqualTo(null)
+        then(databaseRepository).shouldHaveNoInteractions()
+    }
+
+    @Test
+    fun `getConsultation - when not found and has no cache - should return null`() {
+        // Given
+        val uuid = UUID.fromString("c29255f2-10ca-4be5-aab1-801ea173337c")
+        given(databaseRepository.getConsultation(uuid)).willReturn(null)
+
+        // When
+        val result = repository.getConsultation(uuid.toString())
+
+        // Then
+        assertThat(result).isEqualTo(null)
+        then(databaseRepository).should(only()).getConsultation(uuid)
+    }
+
+    @Test
+    fun `getConsultation - when not found and has cache - should return null`() {
+        // Given
+        val uuid = UUID.fromString("c29255f2-10ca-4be5-aab1-801ea173337c")
+        given(databaseRepository.getConsultation(uuid)).willReturn(null)
+
+        // When
+        repository.getConsultation(uuid.toString())
+        val result = repository.getConsultation(uuid.toString())
+
+        // Then
+        assertThat(result).isEqualTo(null)
+        then(databaseRepository).should(times(1)).getConsultation(uuid)
+    }
+
+    @Test
+    fun `getConsultation - when found and has no cache - should return parsed dto from databaseRepository`() {
+        // Given
+        val uuid = UUID.fromString("c29255f2-10ca-4be5-aab1-801ea173337c")
+        given(databaseRepository.getConsultation(uuid)).willReturn(consultationDto)
+        given(thematiqueMapper.toDomain(consultationDto)).willReturn(consultation)
+
+        // When
+        val result = repository.getConsultation(uuid.toString())
+
+        // Then
+        assertThat(result).isEqualTo(consultation)
+        then(databaseRepository).should(only()).getConsultation(uuid)
+    }
+
+    @Test
+    fun `getConsultation - when found and has cache - should return parsed dto from cache`() {
+        // Given
+        val uuid = UUID.fromString("c29255f2-10ca-4be5-aab1-801ea173337c")
+        given(databaseRepository.getConsultation(uuid)).willReturn(consultationDto)
+        given(thematiqueMapper.toDomain(consultationDto)).willReturn(consultation)
+
+        // When
+        repository.getConsultation(uuid.toString())
+        val result = repository.getConsultation(uuid.toString())
+
+        // Then
+        assertThat(result).isEqualTo(consultation)
+        then(databaseRepository).should(times(1)).getConsultation(uuid)
+    }
+
+}
