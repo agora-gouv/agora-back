130ec3e7fff1a49fe89862b8b852e5ee8cc86777
Thierry LEE <thierry.leet@gmail.com>
2023-04-13 13:43:30 +0200

    Add RepositoryImpl and its tests

---
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/reponseConsultation/repository/ReponseConsultationMapper.kt
@@ -0,0 +1,18 @@
+package fr.social.gouv.agora.infrastructure.reponseConsultation.repository
+
+import fr.social.gouv.agora.domain.ReponseConsultation
+import fr.social.gouv.agora.infrastructure.reponseConsultation.dto.ReponseConsultationDTO
+import org.springframework.stereotype.Component
+
+@Component
+class ReponseConsultationMapper {
+
+    fun toDomain(dto: ReponseConsultationDTO): ReponseConsultation {
+        return ReponseConsultation(
+            id = dto.id.toString(),
+            questionId = dto.questionId.toString(),
+            choiceId = dto.choiceId.toString(),
+        )
+    }
+
+}
