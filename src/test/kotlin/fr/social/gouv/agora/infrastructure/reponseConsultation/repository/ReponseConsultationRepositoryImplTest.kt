95fbc2e70546c62bf02dd2bcc6fa8be90e3af0d3
Thierry LEE <thierry.leet@gmail.com>
2023-04-14 17:35:15 +0200

    Evict consultation cache on insert response

--- src/test/kotlin/fr/social/gouv/agora/infrastructure/reponseConsultation/repository/ReponseConsultationRepositoryImplTest.kt
+++ src/test/kotlin/fr/social/gouv/agora/infrastructure/reponseConsultation/repository/ReponseConsultationRepositoryImplTest.kt
@@ -2,14 +2,19 @@ package fr.social.gouv.agora.infrastructure.reponseConsultation.repository
 
 import fr.social.gouv.agora.domain.ReponseConsultationInserting
 import fr.social.gouv.agora.infrastructure.reponseConsultation.dto.ReponseConsultationDTO
+import fr.social.gouv.agora.infrastructure.reponseConsultation.repository.GetReponseConsultationRepositoryImpl.Companion.REPONSE_CONSULTATION_CACHE_NAME
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.*
+import org.mockito.Mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.cache.Cache
+import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.util.*
@@ -28,6 +33,17 @@ internal class ReponseConsultationRepositoryImplTest {
 
     @MockBean
     private lateinit var reponseConsultationMapper: ReponseConsultationMapper
+
+    @MockBean
+    private lateinit var cacheManager: CacheManager
+
+    @MockBean
+    private lateinit var cache: Cache
+
+    @BeforeEach
+    fun setUp() {
+        given(cacheManager.getCache(REPONSE_CONSULTATION_CACHE_NAME)).willReturn(cache)
+    }
 
     private val reponseConsultationDTO = ReponseConsultationDTO(
         id = UUID.fromString("c29255f2-10ca-4be5-aab1-801ea1733302"),
@@ -66,7 +82,7 @@ internal class ReponseConsultationRepositoryImplTest {
     )
 
     @Test
-    fun `insertReponseConsultation - when reponseConsultationId is not null - should return reponse insérée avec succès!`() {
+    fun `insertReponseConsultation - when reponseConsultationId is not null - should return INSERT_SUCCESS`() {
         //Given
         given(reponseConsultationDatabaseRepository.save(reponseConsultationDTO)).willReturn(reponseConsultationDTO)
         given(reponseConsultationMapper.toDto(reponseConsultation)).willReturn(listOf(reponseConsultationDTO))
@@ -76,10 +92,12 @@ internal class ReponseConsultationRepositoryImplTest {
 
         // Then
         assertThat(result).isEqualTo(InsertStatus.INSERT_SUCCESS)
+        then(cacheManager).should(only()).getCache(REPONSE_CONSULTATION_CACHE_NAME)
+        then(cache).should(only()).evict(reponseConsultation.consultationId)
     }
 
     @Test
-    fun `insertReponseConsultation - when reponseConsultation exist in database- should return la reponse existe déjà en base de données`() {
+    fun `insertReponseConsultation - when reponseConsultation exist in database - should return INSERT_CONFLICT`() {
         //Given
         given(reponseConsultationMapper.toDto(reponseConsultation)).willReturn(listOf(reponseConsultationDTO))
         given(reponseConsultationDatabaseRepository.existsById(reponseConsultationDTO.id)).willReturn(true)
@@ -89,24 +107,23 @@ internal class ReponseConsultationRepositoryImplTest {
 
         // Then
         assertThat(result).isEqualTo(InsertStatus.INSERT_CONFLICT)
+        then(cacheManager).shouldHaveNoInteractions()
     }
 
     @Test
-    fun `insertReponseConsultation - when choiceIds is null - should return reponse insérée avec succès!`() {
+    fun `insertReponseConsultation - when choiceIds is null - should return INSERT_SUCCESS`() {
         //Given
-        given(reponseConsultationDatabaseRepository.save(reponseConsultationSansChoiceDTO)).willReturn(
-            reponseConsultationSansChoiceDTO
-        )
-        given(reponseConsultationMapper.toDto(reponseConsultationSansChoice)).willReturn(
-            listOf(
-                reponseConsultationSansChoiceDTO
-            )
-        )
+        given(reponseConsultationDatabaseRepository.save(reponseConsultationSansChoiceDTO))
+            .willReturn(reponseConsultationSansChoiceDTO)
+        given(reponseConsultationMapper.toDto(reponseConsultationSansChoice))
+            .willReturn(listOf(reponseConsultationSansChoiceDTO))
 
         // When
         val result = reponseConsultationRepository.insertReponseConsultation(reponseConsultationSansChoice)
 
         // Then
         assertThat(result).isEqualTo(InsertStatus.INSERT_SUCCESS)
+        then(cacheManager).should(only()).getCache(REPONSE_CONSULTATION_CACHE_NAME)
+        then(cache).should(only()).evict(reponseConsultation.consultationId)
     }
 }
