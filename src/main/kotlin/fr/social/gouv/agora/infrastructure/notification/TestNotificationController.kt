1e71a37bf29b3ac79266300eba63dd15cbf70ba7
Thierry LEE <thierry.leet@gmail.com>
2023-04-28 11:20:16 +0200

    Add test notification endpoint

---
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/notification/TestNotificationController.kt
@@ -0,0 +1,47 @@
+package fr.social.gouv.agora.infrastructure.notification
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.MulticastMessage
+import com.google.firebase.messaging.Notification
+import fr.social.gouv.agora.infrastructure.login.repository.LoginDatabaseRepository
+import fr.social.gouv.agora.infrastructure.supportQag.repository.SupportQagDatabaseRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+
+@RestController
+@Suppress("unused")
+class TestNotificationController(
+    private val supportQagDatabaseRepository: SupportQagDatabaseRepository,
+    private val loginDatabaseRepository: LoginDatabaseRepository,
+) {
+
+    @GetMapping("/testNotification")
+    fun testNotification(): ResponseEntity<String> {
+
+        val retraiteQagId = UUID.fromString("f29c5d6f-9838-4c57-a7ec-0612145bb0c8")
+        val supportQagDTOs = supportQagDatabaseRepository.getSupportQagList(retraiteQagId)
+
+        supportQagDTOs.mapNotNull { supportQagDTO ->
+            loginDatabaseRepository.getUser(supportQagDTO.userId)?.fcmToken.takeUnless { it.isNullOrEmpty() }
+        }.takeUnless { it.isEmpty() }?.let { fcmTokens ->
+            val message = MulticastMessage.builder()
+                .setNotification(
+                    Notification.builder()
+                        .setTitle("Merci pour votre soutien ! ^^")
+                        .setBody("Merci de soutenir la question d'Harry P. à propos de la retraite !")
+                        .build()
+                )
+                .addAllTokens(fcmTokens)
+                .build()
+
+            val response = FirebaseMessaging.getInstance().sendMulticast(message)
+            return ResponseEntity.ok("Sent message to ${response.successCount} successfully!")
+        }
+
+        return ResponseEntity.status(400).body("No fcmToken found")
+    }
+
+}
