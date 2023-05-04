cea9af831403b80415b921b373784b22fb501e01
imaneryane <imane.ryane@octo.com>
2023-05-04 09:58:11 +0200

    debouchonner 76

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/responseQag/ResponseQagListJson.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/responseQag/ResponseQagListJson.kt
@@ -7,6 +7,8 @@ import fr.social.gouv.agora.infrastructure.thematique.ThematiqueJson
 data class ResponseQagListJson(
     @JsonProperty("responses")
     val responsesList: List<ResponseQagPreviewJson>,
+    @JsonProperty("qags")
+    val qagList: QagListJson,
 )
 
 data class ResponseQagPreviewJson(
@@ -22,4 +24,28 @@ data class ResponseQagPreviewJson(
     val authorPortraitUrl: String,
     @JsonProperty("responseDate")
     val responseDate: String,
+)
+
+data class QagListJson(
+    @JsonProperty("popular")
+    val popular: List<QagPopularJson>,
+    @JsonProperty("latest")
+    val latest: List<QagLatestJson>,
+    @JsonProperty("supporting")
+    val supporting: List<QagSupportingJson>,
+)
+
+data class QagPopularJson(
+    @JsonProperty("qagId")
+    val qagId: String,
+)
+
+data class QagLatestJson(
+    @JsonProperty("qagId")
+    val qagId: String,
+)
+
+data class QagSupportingJson(
+    @JsonProperty("qagId")
+    val qagId: String,
 )
