8ae01f0870d05cfe0e4da1176c2a8465714aad9b
Thierry LEE <thierry.leet@gmail.com>
2023-05-15 13:55:44 +0200

    Move configuration class to dedicated files

---
+++ src/main/kotlin/fr/social/gouv/agora/config/CacheConfig.kt
@@ -0,0 +1,56 @@
+package fr.social.gouv.agora.config
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.net.URI
+
+@Configuration
+@Suppress("unused")
+@EnableCaching
+class CacheConfig {
+    companion object {
+        const val DEFAULT_REDIS_USER = "default"
+        const val DEFAULT_REDIS_PORT = 6379
+    }
+
+    @Bean
+    fun getConnectionFactory(): JedisConnectionFactory {
+        val config = RedisStandaloneConfiguration()
+        System.getenv("REDIS_URL")?.let { redisUrl ->
+            try {
+                val redisURI = URI.create(redisUrl)
+                val userInfo = redisURI.userInfo.split(":")
+                config.username = userInfo[0].takeUnless { it.isEmpty() } ?: DEFAULT_REDIS_USER
+                config.setPassword(userInfo[1])
+                config.hostName = redisURI.host
+                config.port = redisURI.port
+            } catch (e: IllegalArgumentException) {
+                println("Invalid Redis URL: $redisUrl")
+            }
+        }
+        return JedisConnectionFactory(config)
+    }
+
+    @Bean
+    fun cacheManager(factory: RedisConnectionFactory): CacheManager {
+        val config = RedisCacheConfiguration.defaultCacheConfig()
+        val redisCacheConfiguration = config
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            )
+        return RedisCacheManager.builder(factory).cacheDefaults(redisCacheConfiguration).build()
+    }
+}
