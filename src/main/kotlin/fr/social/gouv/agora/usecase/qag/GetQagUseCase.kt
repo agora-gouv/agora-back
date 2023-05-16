691396159407eddf576d95f35703de76dda8f80c
Thierry LEE <thierry.leet@gmail.com>
2023-05-16 11:06:16 +0200

    Fix infinite supports on QaG

--- src/main/kotlin/fr/social/gouv/agora/usecase/qag/GetQagUseCase.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/qag/GetQagUseCase.kt
@@ -2,6 +2,7 @@ package fr.social.gouv.agora.usecase.qag
 
 import fr.social.gouv.agora.domain.Qag
 import fr.social.gouv.agora.usecase.feedbackQag.repository.GetFeedbackQagRepository
+import fr.social.gouv.agora.usecase.login.repository.LoginRepository
 import fr.social.gouv.agora.usecase.qag.repository.QagInfoRepository
 import fr.social.gouv.agora.usecase.responseQag.repository.ResponseQagRepository
 import fr.social.gouv.agora.usecase.supportQag.repository.GetSupportQagRepository
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class GetQagUseCase(
+    private val loginRepository: LoginRepository,
     private val qagInfoRepository: QagInfoRepository,
     private val qagSupportQagRepository: GetSupportQagRepository,
     private val responseQagRepository: ResponseQagRepository,
@@ -17,22 +19,27 @@ class GetQagUseCase(
     private val thematiqueRepository: ThematiqueRepository,
 ) {
     fun getQag(qagId: String, deviceId: String): Qag? {
-        // TODO transform deviceId to userId
         return qagInfoRepository.getQagInfo(qagId)?.let { qagInfo ->
-            thematiqueRepository.getThematique(qagInfo.thematiqueId)?.let { thematique ->
-                Qag(
-                    id = qagInfo.id,
-                    thematique = thematique,
-                    title = qagInfo.title,
-                    description = qagInfo.description,
-                    date = qagInfo.date,
-                    status = qagInfo.status,
-                    username = qagInfo.username,
-                    support = qagSupportQagRepository.getSupportQag(qagId = qagId, userId = deviceId),
-                    response = responseQagRepository.getResponseQag(qagId = qagId),
-                    feedback = getFeedbackQagRepository.getFeedbackQagStatus(qagId = qagId, userId = deviceId)
-                )
+            loginRepository.getUser(deviceId)?.let { userInfo ->
+                thematiqueRepository.getThematique(qagInfo.thematiqueId)?.let { thematique ->
+                    Qag(
+                        id = qagInfo.id,
+                        thematique = thematique,
+                        title = qagInfo.title,
+                        description = qagInfo.description,
+                        date = qagInfo.date,
+                        status = qagInfo.status,
+                        username = qagInfo.username,
+                        support = qagSupportQagRepository.getSupportQag(qagId = qagId, userId = userInfo.userId),
+                        response = responseQagRepository.getResponseQag(qagId = qagId),
+                        feedback = getFeedbackQagRepository.getFeedbackQagStatus(
+                            qagId = qagId,
+                            userId = userInfo.userId,
+                        )
+                    )
+                }
             }
+
         }
     }
 
