2b533c29d4bf2bdf10c52e84354657c0b7f9d4a1
Thierry LEE <thierry.leet@gmail.com>
2023-05-16 11:41:47 +0200

    Remove unnecessary calls to LoginRepository

--- src/main/kotlin/fr/social/gouv/agora/usecase/qag/GetQagLatestPreviewListUseCase.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/qag/GetQagLatestPreviewListUseCase.kt
@@ -1,7 +1,6 @@
 package fr.social.gouv.agora.usecase.qag
 
 import fr.social.gouv.agora.domain.QagPreview
-import fr.social.gouv.agora.usecase.login.repository.LoginRepository
 import fr.social.gouv.agora.usecase.qag.repository.QagLatestListRepository
 import fr.social.gouv.agora.usecase.supportQag.repository.GetSupportQagRepository
 import fr.social.gouv.agora.usecase.thematique.repository.ThematiqueRepository
@@ -9,26 +8,23 @@ import org.springframework.stereotype.Service
 
 @Service
 class GetQagLatestPreviewListUseCase(
-    private val loginRepository: LoginRepository,
     private val thematiqueRepository: ThematiqueRepository,
     private val supportRepository: GetSupportQagRepository,
     private val qagLatestListRepository: QagLatestListRepository,
 ) {
-    fun getQagLatestPreviewList(deviceId: String, thematiqueId: String?): List<QagPreview> {
+    fun getQagLatestPreviewList(userId: String, thematiqueId: String?): List<QagPreview> {
         return qagLatestListRepository.getQagLatestList(thematiqueId = thematiqueId.takeUnless { it.isNullOrBlank() })
             .mapNotNull { qagInfo ->
                 thematiqueRepository.getThematique(qagInfo.thematiqueId)?.let { thematique ->
-                    loginRepository.getUser(deviceId)?.let { userInfo ->
-                        supportRepository.getSupportQag(qagInfo.id, userInfo.userId)?.let { supportQag ->
-                            QagPreview(
-                                id = qagInfo.id,
-                                thematique = thematique,
-                                title = qagInfo.title,
-                                username = qagInfo.username,
-                                date = qagInfo.date,
-                                support = supportQag,
-                            )
-                        }
+                    supportRepository.getSupportQag(qagInfo.id, userId)?.let { supportQag ->
+                        QagPreview(
+                            id = qagInfo.id,
+                            thematique = thematique,
+                            title = qagInfo.title,
+                            username = qagInfo.username,
+                            date = qagInfo.date,
+                            support = supportQag,
+                        )
                     }
                 }
             }.sortedByDescending { it.date }
