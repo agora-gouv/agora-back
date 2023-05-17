fc3b9400f910f28ba27f605e51bb8bfa2562143c
Thierry LEE <thierry.leet@gmail.com>
2023-05-17 09:18:10 +0200

    Fix consultation answered deviceId -> userId migration

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/consultation/repository/ConsultationPreviewAnsweredRepositoryImpl.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/consultation/repository/ConsultationPreviewAnsweredRepositoryImpl.kt
@@ -5,7 +5,7 @@ import fr.social.gouv.agora.infrastructure.consultation.dto.ConsultationDTO
 import fr.social.gouv.agora.infrastructure.consultation.repository.ConsultationAnsweredCacheRepository.CacheResult
 import fr.social.gouv.agora.usecase.consultation.repository.ConsultationPreviewAnsweredRepository
 import org.springframework.stereotype.Component
-import java.util.UUID
+import java.util.*
 
 @Component
 class ConsultationPreviewAnsweredRepositoryImpl(
@@ -14,20 +14,13 @@ class ConsultationPreviewAnsweredRepositoryImpl(
     private val mapper: ConsultationPreviewAnsweredMapper,
 ) : ConsultationPreviewAnsweredRepository {
 
-    override fun getConsultationAnsweredList(userId: String?): List<ConsultationPreviewAnsweredInfo> {
+    override fun getConsultationAnsweredList(userId: String): List<ConsultationPreviewAnsweredInfo> {
         return try {
-            val userIdUUID = userId?.let { UUID.fromString(userId) }
-            val consultationAnsweredDTOList =
-                if (userIdUUID != null) {
-                    when (val cacheResult =
-                        consultationAnsweredCacheRepository.getConsultationAnsweredList(userIdUUID)) {
-                        CacheResult.CacheNotInitialized -> getConsultationAnsweredListFromDatabase(userIdUUID)
-                        is CacheResult.CachedConsultationAnsweredList -> cacheResult.consultationAnsweredListDTO
-                    }
-                } else {
-                    emptyList()
-                }
-            consultationAnsweredDTOList.map { dto -> mapper.toDomain(dto) }
+            val userUUID = UUID.fromString(userId)
+            return when (val cacheResult = consultationAnsweredCacheRepository.getConsultationAnsweredList(userUUID)) {
+                CacheResult.CacheNotInitialized -> getConsultationAnsweredListFromDatabase(userUUID)
+                is CacheResult.CachedConsultationAnsweredList -> cacheResult.consultationAnsweredListDTO
+            }.map { dto -> mapper.toDomain(dto) }
         } catch (e: IllegalArgumentException) {
             emptyList()
         }
