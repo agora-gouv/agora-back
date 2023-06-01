53d3fcb6d1b90e37ab72ff5a792509bdd24a43e6
Thierry LEE <thierry.leet@gmail.com>
2023-06-01 16:24:11 +0200

    Remove old moderating repository

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/qag/repository/QagInfoDatabaseRepository.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/qag/repository/QagInfoDatabaseRepository.kt
@@ -12,19 +12,4 @@ interface QagInfoDatabaseRepository : CrudRepository<QagDTO, UUID> {
     @Query(value = "SELECT * FROM qags", nativeQuery = true)
     fun getAllQagList(): List<QagDTO>
 
-    @Query(
-        value = """
-            SELECT * FROM qags where status = 0 
-            AND id NOT IN (SELECT qag_id from responses_qag)  
-            ORDER BY post_date ASC LIMIT 10
-        """,
-        nativeQuery = true
-    )
-    fun getQagModeratingList(): List<QagDTO>
-
-    @Query(
-        value = "SELECT count(*) FROM qags where status = 0 AND id NOT IN (SELECT qag_id from responses_qag) ",
-        nativeQuery = true
-    )
-    fun getModeratingQagCount(): Int
 }
--- src/main/kotlin/fr/social/gouv/agora/usecase/qag/repository/QagModeratingListRepository.kt
+++
@@ -1,6 +0,0 @@
-package fr.social.gouv.agora.usecase.qag.repository
-
-interface QagModeratingListRepository {
-    fun getQagModeratingList(): List<QagModeratingInfo>
-    fun getModeratingQagCount(): Int
-}
