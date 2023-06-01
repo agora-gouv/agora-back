b9c1ffe86c407149078cddb38942c7ef1823f434
Thierry LEE <thierry.leet@gmail.com>
2023-06-01 11:28:23 +0200

    Port Supported QaG preview to new cache system

--- src/main/kotlin/fr/social/gouv/agora/usecase/qag/GetQagSupportedPreviewListUseCase.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/qag/GetQagSupportedPreviewListUseCase.kt
@@ -1,7 +1,12 @@
 package fr.social.gouv.agora.usecase.qag
 
 import fr.social.gouv.agora.domain.QagPreview
-import fr.social.gouv.agora.usecase.qag.repository.QagSupportedListRepository
+import fr.social.gouv.agora.domain.SupportQagInfo
+import fr.social.gouv.agora.domain.Thematique
+import fr.social.gouv.agora.infrastructure.utils.CollectionUtils.mapNotNullWhile
+import fr.social.gouv.agora.usecase.qag.repository.QagInfo
+import fr.social.gouv.agora.usecase.qag.repository.QagInfoRepository
+import fr.social.gouv.agora.usecase.responseQag.repository.ResponseQagRepository
 import fr.social.gouv.agora.usecase.supportQag.repository.GetSupportQagRepository
 import fr.social.gouv.agora.usecase.thematique.repository.ThematiqueRepository
 import org.springframework.stereotype.Service
@@ -10,25 +15,77 @@ import org.springframework.stereotype.Service
 class GetQagSupportedPreviewListUseCase(
     private val thematiqueRepository: ThematiqueRepository,
     private val supportRepository: GetSupportQagRepository,
-    private val qagSupportedListRepository: QagSupportedListRepository,
+    private val qagInfoRepository: QagInfoRepository,
+    private val responseQagRepository: ResponseQagRepository,
+    private val mapper: QagPreviewMapper,
 ) {
-    fun getQagSupportedPreviewList(userId: String, thematiqueId: String?): List<QagPreview> {
-        return qagSupportedListRepository.getQagSupportedList(
-            thematiqueId = thematiqueId.takeUnless { it.isNullOrBlank() },
-            userId = userId,
-        ).mapNotNull { qagInfo ->
-            thematiqueRepository.getThematique(qagInfo.thematiqueId)?.let { thematique ->
-                supportRepository.getSupportQag(qagId = qagInfo.id, userId = userId)?.let { supportQag ->
-                    QagPreview(
-                        id = qagInfo.id,
-                        thematique = thematique,
-                        title = qagInfo.title,
-                        username = qagInfo.username,
-                        date = qagInfo.date,
-                        support = supportQag,
-                    )
-                }
-            }
-        }.sortedByDescending { it.date }
+
+    companion object {
+        private const val MAX_PREVIEW_LIST_SIZE = 10
     }
+
+    fun getQagSupportedPreviewList(userId: String, thematiqueId: String?): List<QagPreview> {
+        val thematiqueMap = mutableMapOf<String, Thematique?>()
+
+        val allQagInfo = qagInfoRepository.getAllQagInfo()
+        val allSupportQag = supportRepository.getAllSupportQag()
+        val allQagAndSupport = allQagInfo.mapNotNull { qagInfo ->
+            val supportQagInfoList = allSupportQag.filter { supportQagInfo -> supportQagInfo.qagId == qagInfo.id }
+            supportQagInfoList.find { supportQagInfo -> supportQagInfo.userId == userId }
+                ?.let { userSupportQagInfo -> QagInfoAndSupport(qagInfo, userSupportQagInfo, supportQagInfoList) }
+        }
+
+        return allQagAndSupport
+            .filter { qagInfoAndSupport -> thematiqueId == null || qagInfoAndSupport.qagInfo.thematiqueId == thematiqueId }
+            .sortedByDescending { qagInfoAndSupport -> qagInfoAndSupport.userSupportQagInfo.supportDate }
+            .mapNotNullWhile(
+                transformMethod = { qagInfoAndSupport ->
+                    toQagPreview(
+                        qagInfo = qagInfoAndSupport.qagInfo,
+                        supportQagList = qagInfoAndSupport.supportQagInfoList,
+                        userId = userId,
+                        thematiqueMap = thematiqueMap
+                    )
+                },
+                loopWhileCondition = { qagPreviewList -> qagPreviewList.size < MAX_PREVIEW_LIST_SIZE }
+            )
+    }
+
+    private fun toQagPreview(
+        qagInfo: QagInfo,
+        supportQagList: List<SupportQagInfo>,
+        userId: String,
+        thematiqueMap: MutableMap<String, Thematique?>
+    ): QagPreview? {
+        return if (responseQagRepository.getResponseQag(qagId = qagInfo.id) != null) null
+        else {
+            getThematique(qagInfo.thematiqueId, thematiqueMap)?.let { thematique ->
+                mapper.toPreview(
+                    qagInfo = qagInfo,
+                    thematique = thematique,
+                    supportQagInfoList = supportQagList,
+                    userId = userId,
+                )
+            }
+        }
+    }
+
+    private fun getThematique(
+        thematiqueId: String,
+        thematiqueMap: MutableMap<String, Thematique?>,
+    ): Thematique? {
+        return if (thematiqueMap.containsKey(thematiqueId)) {
+            thematiqueMap[thematiqueId]
+        } else {
+            thematiqueRepository.getThematique(thematiqueId).also { thematique ->
+                thematiqueMap[thematiqueId] = thematique
+            }
+        }
+    }
+
+    private data class QagInfoAndSupport(
+        val qagInfo: QagInfo,
+        val userSupportQagInfo: SupportQagInfo,
+        val supportQagInfoList: List<SupportQagInfo>,
+    )
 }
