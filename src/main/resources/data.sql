4a29d6c9be115faeec1feafb413bb9b5cec32f4f
Thierry LEE <thierry.leet@gmail.com>
2023-06-01 16:04:16 +0200

    Fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -58,15 +58,14 @@ INSERT INTO thematiques(id, label, picto) VALUES (
     '\ud83d\udce6'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     'f29c5d6f-9838-4c57-a7ec-0612145bb0c8',
     'Pourquoi avoir créé l’application Agora ?',
     'Quel est le but de l’application Agora ? A quel besoin des Français et des Françaises espérez-vous qu’elle réponde ? Que signifie son caractère expérimental ?',
     '2023-06-01',
     1,
     'Agora',
-    '30671310-ee62-11ed-a05b-0242ac120003',
-    0
+    '30671310-ee62-11ed-a05b-0242ac120003'
 ) ON CONFLICT DO NOTHING;
 
 INSERT INTO responses_qag(id, author, author_portrait_url, author_description, response_date, video_url, transcription, qag_id) VALUES (
@@ -80,15 +79,14 @@ INSERT INTO responses_qag(id, author, author_portrait_url, author_description, r
     'f29c5d6f-9838-4c57-a7ec-0612145bb0c8'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '1e9d2830-ee68-11ed-a05b-0242ac120003',
     'Je suis à la tête d’une TPE et je suis très inquiète pour l’avenir. Que fait l’Etat pour nous aider ?',
     'Les prévisions de croissance ne sont pas bonnes. La concurrence internationale est particulièrement rude et mes charges explosent. Je me sens particulièrement seule et inquiète. Que se passera-t-il pour mes employés si je mets la clef sous la porte ??',
     '2023-06-02',
     1,
     'Julie',
-    'c97c3afd-1940-4b6d-950a-734b885ee5cb',
-    0
+    'c97c3afd-1940-4b6d-950a-734b885ee5cb'
 ) ON CONFLICT DO NOTHING;
 
 INSERT INTO responses_qag(id, author, author_portrait_url, author_description, response_date, video_url, transcription, qag_id) VALUES (
@@ -102,15 +100,14 @@ INSERT INTO responses_qag(id, author, author_portrait_url, author_description, r
     '1e9d2830-ee68-11ed-a05b-0242ac120003'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '817b5c32-ee69-11ed-a05b-0242ac120003',
     'Pourquoi ne peut-on pas choisir là où vont précisément nos impôts ? ',
     'Il est normal de payer ses impôts pour financer notre système. Néanmoins, il me semble qu’il y aurait un consentement plus grand si vous pouvions chacun choisir les postes de dépense auquel nous aimerions contribuer. Cela donnerait plus de sens et moins l’impression de financer “une machine globale”. Qu’en pensez-vous ?',
     '2023-06-01',
     1,
     'Jean',
-    '30671310-ee62-11ed-a05b-0242ac120003',
-    0
+    '30671310-ee62-11ed-a05b-0242ac120003'
 ) ON CONFLICT DO NOTHING;
 
 INSERT INTO responses_qag(id, author, author_portrait_url, author_description, response_date, video_url, transcription, qag_id) VALUES (
@@ -124,114 +121,104 @@ INSERT INTO responses_qag(id, author, author_portrait_url, author_description, r
     '817b5c32-ee69-11ed-a05b-0242ac120003'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '996436ca-ee69-11ed-a05b-0242ac120003',
     'On parle beaucoup de l’intelligence artificielle… vous comptez faire quoi pour les emplois qu’elle risque de remplacer ?',
     'ChatGPT est arrivé avec fureur et on voit déjà combien un certain nombre de tâches et de métier pourraient être rapidement remplacés par l’intelligence artificielle… Faut-il interdire ChatGPT? Faut-il ré-orienter les travailleurs concernés ?',
     '2023-06-02',
     1,
     'Nina',
-    '1f3dbdc6-cff7-4d6a-88b5-c5ec84c55d15',
-    0
+    '1f3dbdc6-cff7-4d6a-88b5-c5ec84c55d15'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     'f8776dd0-ee69-11ed-a05b-0242ac120003',
     'Pourquoi ne taxe-t-on pas le kérosène des avions ? La France doit militer pour !',
     'Je trouve ça absolument lunaire que le kérosène des avions ne soit pas taxés, alors que les carburants des autres modes de transports le sont. Il s’agit d’une mesure à prendre au niveau international : la France défend-elle cette position de pur bon sens et quand peut-on espérer avoir des résultats ?',
     '2023-06-01',
     1,
     'Elliott',
-    'bb051bf2-644b-47b6-9488-7759fa727dc0',
-    0
+    'bb051bf2-644b-47b6-9488-7759fa727dc0'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '40c04882-ee6a-11ed-a05b-0242ac120003',
     'Peut-on refuser d’être pris en charge par un soignant non-vacciné ? Personnellement, ça me dérange.',
     'Au pays de Pasteur, je trouve ça inadmissible que les soignants non-vaccinés soient ré-intégrés dans le corps hospitalier. Est-ce possible de refuser qu’un tel soignant me soigne ? Désolée mais je ne fais pas confiance à quelqu’un qui ne croit pas à la science.',
     '2023-06-01',
     1,
     'Laure',
-    'a4bb4b27-3271-4278-83c9-79ac3eee843a',
-    0
+    'a4bb4b27-3271-4278-83c9-79ac3eee843a'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '8171c50e-ee6a-11ed-a05b-0242ac120003',
     'Quel va être l’impact de la baisse de la note de la France par l’agence de notation financière Fitch ?',
     'L’agence de notation financière Fitch a baissé la note de la France, passant de AA à AA-. Qu’est-ce que ça veut dire concrètement ? Est-ce un signal pour l’avenir ou cela a-t-il un impact financier direct ?',
     '2023-06-02',
     1,
     'Patrick',
-    'c97c3afd-1940-4b6d-950a-734b885ee5cb',
-    0
+    'c97c3afd-1940-4b6d-950a-734b885ee5cb'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '1731a370-ee6b-11ed-a05b-0242ac120003',
     'Ma question, c’est pourquoi on n’arrive pas, dans ce pays, à recruter davantage de professeurs ?!',
     'Il y a bien sûr la question de la rémunération, mais au-delà de ça, c’est aussi la position sociale et la reconnaissance même du professeur qui semble avoir “baissé” ces 20 dernières années. Comment remédier à cela ?',
     '2023-06-02',
     1,
     'Agny',
-    '5b9180e6-3e43-4c63-bcb5-4eab621fc016',
-    0
+    '5b9180e6-3e43-4c63-bcb5-4eab621fc016'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '5c5f5460-ee6b-11ed-a05b-0242ac120003',
     'Aurait-on pu faire quelque chose pour éviter la guerre en Ukraine ? Et les prochaines ? Je m’interroge',
     'La guerre en Ukraine semble partie pour durer, avec les conséquences dramatiques que l’on sait. Aurait-on pu éviter cette guerre ? Peut-on faire quelque chose pour éviter qu’il y en ait d’autres ?',
     '2023-06-02',
     1,
     'Béatrice',
-    '8e200137-df3b-4bde-9981-b39a3d326da7',
-    0
+    '8e200137-df3b-4bde-9981-b39a3d326da7'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '96e8341c-ee6b-11ed-a05b-0242ac120003',
     'Pourquoi ne pas tester un tarif unique de transport en commun pas trop cher partout en France comme en Allemagne ?',
     'L’Allemagne a lancé en mai DeutschlandTicket à 49 euros par mois.qui permet avec un seul billet d’aller dans n’importe quel transport en commun local et régional. Ne serait-ce pas un bon coup de pouce pour le pouvoir d’achat et la promotion des transports en commun ?',
     '2023-06-01',
     1,
     'Danny',
-    '0f644115-08f3-46ff-b776-51f19c65fdd1',
-    0
+    '0f644115-08f3-46ff-b776-51f19c65fdd1'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     'd09f2788-ee6b-11ed-a05b-0242ac120003',
     'La France devrait autoriser la reconnaissance faciale par vidéo-surveillance pour lutter contre les délits du quotidien : qu’est-ce qu’on attend ?',
     'Y-a-t-il eu des expérimentations sur ces dispositifs ? Est-ce que c’est efficace ? Est-ce que les JO vont être l’occasion de tester des choses sur ce sujet ?',
     '2023-06-02',
     1,
     'Josette',
-    'b276606e-f251-454e-9a73-9b70a6f30bfd',
-    0
+    'b276606e-f251-454e-9a73-9b70a6f30bfd'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '1cde43fe-ee6c-11ed-a05b-0242ac120003',
     'Est-ce qu’il ne serait pas temps d’avoir enfin (!) un vrai débat sur la légalisation du cannabis, comme ils le font dans plein de pays ?',
     'On consomme beaucoup de cannabis en France, même si c’est illégal. Ne devrait-on pas avoir un débat sur une potentielle légalisation pour que cette réalité cesse de nourrir des trafics illégaux ? Arrêtons l’hypocrisie ! (Et ce serait sûrement bon pour les finances de l’Etat …)',
     '2023-06-02',
     1,
     'Tyron',
-    '47897e51-8e94-4920-a26a-1b1e5e232e82',
-    0
+    '47897e51-8e94-4920-a26a-1b1e5e232e82'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO qags(id, title, description, post_date, status, username, thematique_id, is_accepted) VALUES (
+INSERT INTO qags(id, title, description, post_date, status, username, thematique_id) VALUES (
     '59028e08-ee6c-11ed-a05b-0242ac120003',
     'Quel est le niveau de la menace terroriste sur la France aujourd’hui ?',
     'On se doute qu’il y a des attentats déjoués dont nous ne sommes pas au courant et c’est bien normal. Mais pouvez-vous nous dire par rapport aux années précédentes et des événements dans le monde actuel à quel niveau de menace nous situons-nous ?',
     '2023-06-01',
     1,
     'Kevin',
-    'b276606e-f251-454e-9a73-9b70a6f30bfd',
-    0
+    'b276606e-f251-454e-9a73-9b70a6f30bfd'
 ) ON CONFLICT DO NOTHING;
 
 INSERT INTO consultations(id, title, end_date, cover_url, question_count, estimated_time, participant_count_goal, description, tips_description, thematique_id) VALUES (
