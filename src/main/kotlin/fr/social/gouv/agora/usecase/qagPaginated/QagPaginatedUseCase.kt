9578bdcad0cb7b90bdb66a8eb414c9f181e279e2
Thierry LEE <thierry.leet@gmail.com>
2023-06-02 15:59:48 +0200

    Implement paginated API without date freeze

---
+++ src/main/kotlin/fr/social/gouv/agora/usecase/qagPaginated/QagPaginatedUseCase.kt
@@ -0,0 +1,93 @@
+package fr.social.gouv.agora.usecase.qagPaginated
+
+import fr.social.gouv.agora.domain.QagPreview
+import fr.social.gouv.agora.usecase.qag.GetQagWithSupportAndThematiqueUseCase
+import fr.social.gouv.agora.usecase.qag.QagFilters
+import fr.social.gouv.agora.usecase.qag.QagInfoWithSupportAndThematique
+import fr.social.gouv.agora.usecase.qag.QagPreviewMapper
+import org.springframework.stereotype.Service
+import java.lang.Integer.min
+import kotlin.math.ceil
+
+@Service
+class QagPaginatedUseCase(
+    private val getQagListUseCase: GetQagWithSupportAndThematiqueUseCase,
+    private val filterGenerator: QagFilterGenerator,
+    private val mapper: QagPreviewMapper,
+) {
+
+    companion object {
+        private const val MAX_PAGE_LIST_SIZE = 20
+    }
+
+    fun getPopularQagPaginated(userId: String, pageNumber: Int, thematiqueId: String?): QagsAndMaxPageCount? {
+        return getQagPaginated(
+            userId = userId,
+            pageNumber = pageNumber,
+            qagFilters = filterGenerator.getPaginatedQagFilters(
+                userId = userId,
+                pageNumber = pageNumber,
+                thematiqueId = thematiqueId,
+            ),
+            sortByDescendingSelector = { qag -> qag.supportQagInfoList.size },
+        )
+    }
+
+    fun getLatestQagPaginated(userId: String, pageNumber: Int, thematiqueId: String?): QagsAndMaxPageCount? {
+        return getQagPaginated(
+            userId = userId,
+            pageNumber = pageNumber,
+            qagFilters = filterGenerator.getPaginatedQagFilters(
+                userId = userId,
+                pageNumber = pageNumber,
+                thematiqueId = thematiqueId,
+            ),
+            sortByDescendingSelector = { qag -> qag.qagInfo.date },
+        )
+    }
+
+    fun getSupportedQagPaginated(userId: String, pageNumber: Int, thematiqueId: String?): QagsAndMaxPageCount? {
+        return getQagPaginated(
+            userId = userId,
+            pageNumber = pageNumber,
+            qagFilters = filterGenerator.getSupportedPaginatedQagFilters(
+                userId = userId,
+                pageNumber = pageNumber,
+                thematiqueId = thematiqueId,
+            ),
+            sortByDescendingSelector = { qag ->
+                qag.supportQagInfoList.find { supportQagInfo -> supportQagInfo.userId == userId }?.supportDate
+            },
+        )
+    }
+
+    private fun <R : Comparable<R>> getQagPaginated(
+        userId: String,
+        pageNumber: Int,
+        qagFilters: QagFilters,
+        sortByDescendingSelector: (QagInfoWithSupportAndThematique) -> R?,
+    ): QagsAndMaxPageCount? {
+        if (pageNumber <= 0) return null
+        val qagList = getQagListUseCase.getQagWithSupportAndThematique(qagFilters = qagFilters)
+
+        val minIndex = (pageNumber - 1) * MAX_PAGE_LIST_SIZE
+        if (minIndex > qagList.size) return null
+        val maxIndex = min(pageNumber * MAX_PAGE_LIST_SIZE, qagList.size)
+
+        val qags = qagList
+            .sortedByDescending { qag -> sortByDescendingSelector.invoke(qag) }
+            .subList(fromIndex = minIndex, maxIndex)
+            .map { qag -> mapper.toPreview(qag = qag, userId = userId) }
+
+        return QagsAndMaxPageCount(
+            qags = qags,
+            maxPageCount = ceil(qagList.size.toDouble() / MAX_PAGE_LIST_SIZE.toDouble()).toInt(),
+        )
+    }
+
+}
+
+data class QagsAndMaxPageCount(
+    val qags: List<QagPreview>,
+    val maxPageCount: Int,
+)
