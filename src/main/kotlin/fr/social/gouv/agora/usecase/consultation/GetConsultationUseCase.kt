571b756d73d4d5de4912eccec1fbcabc6681691e
Thierry LEE <thierry.leet@gmail.com>
2023-06-06 18:50:51 +0200

    Implement hasAnswered value on consultation details

--- src/main/kotlin/fr/social/gouv/agora/usecase/consultation/GetConsultationUseCase.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/consultation/GetConsultationUseCase.kt
@@ -2,17 +2,19 @@ package fr.social.gouv.agora.usecase.consultation
 
 import fr.social.gouv.agora.domain.Consultation
 import fr.social.gouv.agora.usecase.consultation.repository.ConsultationInfoRepository
+import fr.social.gouv.agora.usecase.reponseConsultation.repository.GetConsultationResponseRepository
 import fr.social.gouv.agora.usecase.thematique.repository.ThematiqueRepository
 import org.springframework.stereotype.Service
 
 
 @Service
 class GetConsultationUseCase(
-    private val repository: ConsultationInfoRepository,
+    private val consultationInfoRepository: ConsultationInfoRepository,
     private val thematiqueRepository: ThematiqueRepository,
+    private val consultationResponseRepository: GetConsultationResponseRepository,
 ) {
-    fun getConsultation(id: String): Consultation? {
-        return repository.getConsultation(id)?.let { consultationInfo ->
+    fun getConsultation(consultationId: String, userId: String): Consultation? {
+        return consultationInfoRepository.getConsultation(consultationId)?.let { consultationInfo ->
             thematiqueRepository.getThematique(consultationInfo.thematiqueId)?.let { thematique ->
                 Consultation(
                     id = consultationInfo.id,
@@ -26,6 +28,8 @@ class GetConsultationUseCase(
                     description = consultationInfo.description,
                     tipsDescription = consultationInfo.tipsDescription,
                     thematique = thematique,
+                    hasAnswered = consultationResponseRepository.getConsultationResponses(consultationId)
+                        .any { consultationResponse -> consultationResponse.userId == userId }
                 )
             }
         }
