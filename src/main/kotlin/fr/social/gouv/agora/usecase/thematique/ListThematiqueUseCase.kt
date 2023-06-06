1f42e702825ae00825f17dec93de9df12cf1a5b9
imaneryane <imane.ryane@octo.com>
2023-06-06 18:41:23 +0200

    filtrer par ID

--- src/main/kotlin/fr/social/gouv/agora/usecase/thematique/ListThematiqueUseCase.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/thematique/ListThematiqueUseCase.kt
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Service
 
 @Service
 class ListThematiqueUseCase(private val thematiqueRepository: ThematiqueRepository) {
+    companion object {
+        private const val ID_THEMATIQUE_AUTRE = "47897e51-8e94-4920-a26a-1b1e5e232e82"
+    }
 
     fun getThematiqueList(): List<Thematique> {
         val listThematiqueBeforeSort = thematiqueRepository.getThematiqueList()
-        return listThematiqueBeforeSort.sortedWith(compareBy({ it.label == "Autre" }, { it.label }))
+        return listThematiqueBeforeSort.sortedWith(compareBy({ it.id == ID_THEMATIQUE_AUTRE }, { it.label }))
     }
 }
