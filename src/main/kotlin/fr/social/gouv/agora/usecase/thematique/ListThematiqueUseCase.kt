800cfa11a6dada3cee3310eb72afc031d0726fbe
imaneryane <imane.ryane@octo.com>
2023-06-06 16:35:54 +0200

    tri thematiques et autre en fin de liste

--- src/main/kotlin/fr/social/gouv/agora/usecase/thematique/ListThematiqueUseCase.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/thematique/ListThematiqueUseCase.kt
@@ -1,11 +1,14 @@
 package fr.social.gouv.agora.usecase.thematique
 
+import fr.social.gouv.agora.domain.Thematique
 import fr.social.gouv.agora.usecase.thematique.repository.ThematiqueRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ListThematiqueUseCase(private val thematiqueRepository: ThematiqueRepository) {
 
-    fun getThematiqueList() = thematiqueRepository.getThematiqueList()
-
+    fun getThematiqueList(): List<Thematique> {
+        val listThematiqueBeforeSort = thematiqueRepository.getThematiqueList()
+        return listThematiqueBeforeSort.sortedWith(compareBy({ it.label == "Autre" }, { it.label }))
+    }
 }
