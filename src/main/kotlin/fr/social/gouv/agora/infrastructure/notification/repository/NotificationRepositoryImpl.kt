f2d1cdc6a265bc8d724d6dbe67522ec8feb96e50
Thierry LEE <thierry.leet@gmail.com>
2023-06-13 17:10:46 +0200

    Fix error on moderation when fcmToken is malformed

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/notification/repository/NotificationRepositoryImpl.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/notification/repository/NotificationRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package fr.social.gouv.agora.infrastructure.notification.repository
 
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingException
 import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.Notification
 import fr.social.gouv.agora.usecase.notification.repository.NotificationRepository
@@ -8,6 +9,7 @@ import fr.social.gouv.agora.usecase.notification.repository.NotificationResult
 import org.springframework.stereotype.Component
 
 @Component
+@Suppress("unused")
 class NotificationRepositoryImpl : NotificationRepository {
 
     override fun sendNotificationMessage(fcmToken: String, title: String, messageToSend: String): NotificationResult {
@@ -16,11 +18,18 @@ class NotificationRepositoryImpl : NotificationRepository {
                 Notification.builder()
                     .setTitle(title)
                     .setBody(messageToSend)
-                    .build())
+                    .build()
+            )
             .setToken(fcmToken).build()
-        val response = FirebaseMessaging.getInstance().send(message)
-        return if (response.isNullOrEmpty())
+
+        return try {
+            val response = FirebaseMessaging.getInstance().send(message)
+            if (response.isNullOrEmpty())
+                NotificationResult.FAILURE
+            else NotificationResult.SUCCESS
+        } catch (e: FirebaseMessagingException) {
             NotificationResult.FAILURE
-        else NotificationResult.SUCCESS
+        }
     }
+
 }
