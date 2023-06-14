e1cb552321976faba9c4bf4845b64023f98c43e4
Thierry LEE <thierry.leet@gmail.com>
2023-06-14 13:58:40 +0200

    Prevent support QaG when has response or incorrect status

--- src/main/kotlin/fr/social/gouv/agora/usecase/supportQag/DeleteSupportQagUseCase.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/supportQag/DeleteSupportQagUseCase.kt
@@ -1,18 +1,30 @@
 package fr.social.gouv.agora.usecase.supportQag
 
+import fr.social.gouv.agora.domain.QagStatus
 import fr.social.gouv.agora.domain.SupportQagDeleting
+import fr.social.gouv.agora.usecase.qag.repository.QagInfoRepository
+import fr.social.gouv.agora.usecase.responseQag.repository.ResponseQagRepository
 import fr.social.gouv.agora.usecase.supportQag.repository.SupportQagRepository
 import fr.social.gouv.agora.usecase.supportQag.repository.SupportQagResult
 import org.springframework.stereotype.Service
 
 @Service
-class DeleteSupportQagUseCase(private val repository: SupportQagRepository) {
+class DeleteSupportQagUseCase(
+    private val responseQagRepository: ResponseQagRepository,
+    private val qagInfoRepository: QagInfoRepository,
+    private val supportQagRepository: SupportQagRepository,
+) {
     fun deleteSupportQag(supportQagDeleting: SupportQagDeleting): SupportQagResult {
-        return repository.deleteSupportQag(
-            SupportQagDeleting(
-                qagId = supportQagDeleting.qagId,
-                userId = supportQagDeleting.userId,
+        if (responseQagRepository.getResponseQag(supportQagDeleting.qagId) != null) return SupportQagResult.FAILURE
+
+        return when (qagInfoRepository.getQagInfo(supportQagDeleting.qagId)?.status) {
+            null, QagStatus.ARCHIVED, QagStatus.MODERATED_REJECTED -> SupportQagResult.FAILURE
+            QagStatus.OPEN, QagStatus.MODERATED_ACCEPTED -> supportQagRepository.deleteSupportQag(
+                SupportQagDeleting(
+                    qagId = supportQagDeleting.qagId,
+                    userId = supportQagDeleting.userId,
+                )
             )
-        )
+        }
     }
 }
