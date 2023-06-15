42bb9f8944509d13a7cc0bb95f5e3ebc59d76b09
Thierry LEE <thierry.leet@gmail.com>
2023-06-15 13:43:12 +0200

    Add use case

---
+++ src/main/kotlin/fr/social/gouv/agora/usecase/qagSelection/QagSelectForResponseFilterGenerator.kt
@@ -0,0 +1,18 @@
+package fr.social.gouv.agora.usecase.qagSelection
+
+import fr.social.gouv.agora.domain.QagStatus
+import fr.social.gouv.agora.usecase.qag.QagFilters
+import org.springframework.stereotype.Component
+
+@Component
+class QagSelectForResponseFilterGenerator {
+
+    fun getFilter(): QagFilters {
+        return QagFilters(
+            filterQagInfo = { qagInfo -> qagInfo.status == QagStatus.SELECTED_FOR_RESPONSE },
+            filterSupportQagInfo = { true },
+            filterQagWithSupportList = { true },
+        )
+    }
+
+}
