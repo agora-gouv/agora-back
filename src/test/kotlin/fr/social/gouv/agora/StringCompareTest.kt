695cb68aac1f1246fc236527a4c368fcb894d3a3
Thierry LEE <thierry.leet@gmail.com>
2023-07-19 09:50:18 +0200

    Add a sentence normalizer to improve word similarity search

--- src/test/kotlin/fr/social/gouv/agora/StringCompareTest.kt
+++ src/test/kotlin/fr/social/gouv/agora/StringCompareTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.models.word2vec.Word2Vec
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory
 import org.deeplearning4j.text.sentenceiterator.BasicLineIterator
 import java.io.File
@@ -17,25 +17,26 @@ class StringCompareTest {
     @Test
     fun `compareString - when - should `() {
         // Given
-        val sentenceIterator = BasicLineIterator(File(this.javaClass.classLoader.getResource("data.txt")!!.file))
+        val sentenceIterator = BasicLineIterator(File(this.javaClass.classLoader.getResource("data.txt")!!.file)).also {
+            it.preProcessor = SentenceNormalizer()
+        }
         val word2vec = Word2Vec.Builder()
-            .minWordFrequency(1)
+            .minWordFrequency(3)
             .iterations(1)
             .layerSize(100)
             .seed(1337L)
             .windowSize(5)
             .iterate(sentenceIterator)
             .tokenizerFactory(DefaultTokenizerFactory())
+            .learningRate(0.025)
             .build()
         word2vec.fit()
 
         // When
-        val result = word2vec.similarity("vidéo", "surveillance")
-        println("Result = $result")
-
+        val result = word2vec.wordsNearest("politique", 10)
 
         // Then
-        assertThat(true).isFalse
+        assertThat(result).isEqualTo(listOf("coucou"))
     }
 
 }
