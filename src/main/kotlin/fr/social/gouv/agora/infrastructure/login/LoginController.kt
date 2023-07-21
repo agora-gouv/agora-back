cb01878d875990c68a55d7a2271295b914b11b61
Thierry LEE <thierry.leet@gmail.com>
2023-07-21 09:48:42 +0200

    Add AppVersionControl use case on login & signup controllers

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/login/LoginController.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/login/LoginController.kt
@@ -1,5 +1,7 @@
 package fr.social.gouv.agora.infrastructure.login
 
+import fr.social.gouv.agora.usecase.appVersionControl.AppVersionControlUseCase
+import fr.social.gouv.agora.usecase.appVersionControl.AppVersionStatus.*
 import fr.social.gouv.agora.usecase.featureFlags.FeatureFlagsUseCase
 import fr.social.gouv.agora.usecase.login.LoginUseCase
 import jakarta.servlet.http.HttpServletResponse
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @Suppress("unused")
 class LoginController(
+    private val appVersionControlUseCase: AppVersionControlUseCase,
     private val loginUseCase: LoginUseCase,
     private val loginInfoJsonMapper: LoginInfoJsonMapper,
     private val featureFlagsUseCase: FeatureFlagsUseCase,
@@ -20,20 +23,28 @@ class LoginController(
     @PostMapping("/login")
     fun login(
         @RequestHeader("fcmToken") fcmToken: String,
+        @RequestHeader("versionName") versionName: String?,
+        @RequestHeader("versionCode") versionCode: String,
+        @RequestHeader("platform") platform: String,
         @RequestBody loginToken: String,
     ): ResponseEntity<*> {
         if (!featureFlagsUseCase.getFeatureFlags().isLoginEnabled) {
             return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
         }
 
-        return when (val loginTokenResult = LoginTokenGenerator.decodeLoginToken(loginToken)) {
-            DecodeResult.Failure -> ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
-            is DecodeResult.Success -> loginUseCase.login(
-                loginTokenData = loginTokenResult.loginTokenData,
-                fcmToken = fcmToken,
-            )?.let { userInfo ->
-                ResponseEntity.ok().body(loginInfoJsonMapper.toJson(domain = userInfo))
-            } ?: ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
+        return when (appVersionControlUseCase.getAppVersionStatus(platform = platform, versionCode = versionCode)) {
+            INVALID_APP -> ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
+            UPDATE_REQUIRED -> ResponseEntity.status(HttpServletResponse.SC_PRECONDITION_FAILED)
+                .body(Unit)
+            AUTHORIZED -> when (val loginTokenResult = LoginTokenGenerator.decodeLoginToken(loginToken)) {
+                DecodeResult.Failure -> ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
+                is DecodeResult.Success -> loginUseCase.login(
+                    loginTokenData = loginTokenResult.loginTokenData,
+                    fcmToken = fcmToken,
+                )?.let { userInfo ->
+                    ResponseEntity.ok().body(loginInfoJsonMapper.toJson(domain = userInfo))
+                } ?: ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
+            }
         }
     }
 
