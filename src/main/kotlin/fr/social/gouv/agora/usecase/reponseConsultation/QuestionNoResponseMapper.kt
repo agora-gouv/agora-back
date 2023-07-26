9ffd23b34760265b496ae771ec922d196a80cd66
Thierry LEE <thierry.leet@gmail.com>
2023-07-26 11:34:34 +0200

    Add not application response when insert responses with missing questions

--- src/main/kotlin/fr/social/gouv/agora/usecase/reponseConsultation/QuestionNoResponseMapper.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/reponseConsultation/QuestionNoResponseMapper.kt
@@ -1,7 +1,9 @@
 package fr.social.gouv.agora.usecase.reponseConsultation
 
 import fr.social.gouv.agora.domain.*
+import fr.social.gouv.agora.infrastructure.utils.UuidUtils.NOT_APPLICABLE_CHOICE_UUID
 import fr.social.gouv.agora.infrastructure.utils.UuidUtils.NOT_FOUND_UUID_STRING
+import fr.social.gouv.agora.infrastructure.utils.UuidUtils.SKIP_QUESTION_CHOICE_UUID
 import org.springframework.stereotype.Component
 
 @Component
@@ -20,6 +22,9 @@ class QuestionNoResponseMapper {
                 choixPossibleList = questionWithChoices.choixPossibleList + createNoResponseChoice(
                     ordre = questionWithChoices.choixPossibleList.size + 1,
                     questionId = questionWithChoices.id
+                ) + createNotApplicableResponseChoice(
+                    ordre = questionWithChoices.choixPossibleList.size + 2,
+                    questionId = questionWithChoices.id,
                 ),
             )
 
@@ -32,9 +37,25 @@ class QuestionNoResponseMapper {
                 consultationId = questionWithChoices.consultationId,
                 choixPossibleList = questionWithChoices.choixPossibleList + createNoResponseChoice(
                     ordre = questionWithChoices.choixPossibleList.size + 1,
-                    questionId = questionWithChoices.id
+                    questionId = questionWithChoices.id,
+                ) + createNotApplicableResponseChoice(
+                    ordre = questionWithChoices.choixPossibleList.size + 2,
+                    questionId = questionWithChoices.id,
                 ),
                 maxChoices = questionWithChoices.maxChoices
+            )
+
+            is QuestionConditional -> QuestionConditional(
+                id = questionWithChoices.id,
+                title = questionWithChoices.title,
+                popupDescription = questionWithChoices.popupDescription,
+                order = questionWithChoices.order,
+                nextQuestionId = questionWithChoices.nextQuestionId,
+                consultationId = questionWithChoices.consultationId,
+                choixPossibleList = questionWithChoices.choixPossibleList + createNotApplicableResponseConditionalChoice(
+                    ordre = questionWithChoices.choixPossibleList.size + 1,
+                    questionId = questionWithChoices.id,
+                ),
             )
 
             else -> questionWithChoices
@@ -42,9 +63,27 @@ class QuestionNoResponseMapper {
     }
 
     private fun createNoResponseChoice(ordre: Int, questionId: String) = ChoixPossibleDefault(
-        id = NOT_FOUND_UUID_STRING,
+        id = SKIP_QUESTION_CHOICE_UUID,
         label = "Pas de réponse",
         ordre = ordre,
         questionId = questionId,
     )
+
+    private fun createNotApplicableResponseChoice(ordre: Int, questionId: String) = ChoixPossibleDefault(
+        id = NOT_APPLICABLE_CHOICE_UUID,
+        label = "Non applicable",
+        ordre = ordre,
+        questionId = questionId,
+    )
+
+    private fun createNotApplicableResponseConditionalChoice(ordre: Int, questionId: String) =
+        createNotApplicableResponseChoice(ordre, questionId).let {
+            ChoixPossibleConditional(
+                id = it.id,
+                label = it.label,
+                ordre = it.ordre,
+                questionId = it.questionId,
+                nextQuestionId = NOT_FOUND_UUID_STRING,
+            )
+        }
 }
