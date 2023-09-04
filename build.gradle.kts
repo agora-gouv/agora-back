0badfab4fc5276c8e16d7d8e60a9b799b2cfc4c9
Thierry LEE <thierry.leet@gmail.com>
2023-09-04 17:24:38 +0200

    Add content sanitizer when insert QaG

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("redis.clients:jedis")
 	implementation("com.google.firebase:firebase-admin:9.1.1")
+	implementation("com.googlecode.owasp-java-html-sanitizer:owasp-java-html-sanitizer:20220608.1")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
