acd7e1bcc89a11dcf62110fc0b33432a4291f832
Thierry LEE <thierry.leet@gmail.com>
2023-09-05 11:04:38 +0200

    Notification changes on moderate

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/notification/repository/NotificationMessageRepositoryImpl.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/notification/repository/NotificationMessageRepositoryImpl.kt
@@ -13,10 +13,16 @@ class NotificationMessageRepositoryImpl : NotificationMessageRepository {
         private const val QAG_ACCEPTED_TITLE = "Et si vous partagiez votre question ?"
         private const val QAG_ACCEPTED_MESSAGE =
             "Proposez à vos amis de la soutenir pour avoir plus de chance d’obtenir une réponse !"
+        private const val QAG_ACCEPTED_AFTER_REJECT_TITLE = "Bonne nouvelle !"
+        private const val QAG_ACCEPTED_AFTER_REJECT_MESSAGE =
+            "Votre question a été ré-évaluée et correspond à la charte. "
     }
 
     override fun getQagRejectedTitle() = QAG_REJECTED_TITLE
     override fun getQagRejectedMessage() = QAG_REJECTED_MESSAGE
     override fun getQagAcceptedTitle() = QAG_ACCEPTED_TITLE
     override fun getQagAcceptedMessage() = QAG_ACCEPTED_MESSAGE
+    override fun getQagAcceptedAfterRejectTitle() = QAG_ACCEPTED_AFTER_REJECT_TITLE
+    override fun getQagAcceptedAfterRejectMessage() = QAG_ACCEPTED_AFTER_REJECT_MESSAGE
+
 }
