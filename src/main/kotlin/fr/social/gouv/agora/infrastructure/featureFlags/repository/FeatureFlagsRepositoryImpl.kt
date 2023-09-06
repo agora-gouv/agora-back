58f44a6b9bd9a8bda78f84f4b035e572c1b10815
Thierry LEE <thierry.leet@gmail.com>
2023-09-06 12:06:53 +0200

    Rework FeatureFlags so it can be hot swapped

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/featureFlags/repository/FeatureFlagsRepositoryImpl.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/featureFlags/repository/FeatureFlagsRepositoryImpl.kt
@@ -1,21 +1,34 @@
 package fr.social.gouv.agora.infrastructure.featureFlags.repository
 
-import fr.social.gouv.agora.domain.FeatureFlags
+import fr.social.gouv.agora.domain.AgoraFeature
 import fr.social.gouv.agora.usecase.featureFlags.repository.FeatureFlagsRepository
+import org.springframework.cache.CacheManager
 import org.springframework.stereotype.Component
 
 @Component
 @Suppress("unused")
-class FeatureFlagsRepositoryImpl : FeatureFlagsRepository {
+class FeatureFlagsRepositoryImpl(private val cacheManager: CacheManager) : FeatureFlagsRepository {
 
-    override fun getFeatureFlags(): FeatureFlags {
-        return FeatureFlags(
-            isAskQuestionEnabled = System.getenv("IS_ASK_QUESTION_ENABLED").toBoolean(),
-            isSignUpEnabled = System.getenv("IS_SIGNUP_ENABLED").toBoolean(),
-            isLoginEnabled = System.getenv("IS_LOGIN_ENABLED").toBoolean(),
-            isQagSelectEnabled = System.getenv("IS_QAG_SELECT_ENABLED").toBoolean(),
-            isQagArchiveEnabled = System.getenv("IS_QAG_ARCHIVE_ENABLED").toBoolean(),
-        )
+    companion object {
+        private const val FEATURE_FLAGS_CACHE_NAME = "featureFlags"
     }
+
+    override fun isFeatureEnabled(feature: AgoraFeature): Boolean {
+        val featureKey = toKey(feature)
+        val cachedValue = getCache()?.get(featureKey)?.get()?.let { it == true }
+
+        return cachedValue ?: System.getenv(featureKey).toBoolean()
+            .also { featureValue -> getCache()?.put(featureKey, featureValue) }
+    }
+
+    private fun toKey(feature: AgoraFeature) = when (feature) {
+        AgoraFeature.SignUp -> "IS_SIGNUP_ENABLED"
+        AgoraFeature.Login -> "IS_LOGIN_ENABLED"
+        AgoraFeature.AskQuestion -> "IS_ASK_QUESTION_ENABLED"
+        AgoraFeature.QagSelect -> "IS_QAG_SELECT_ENABLED"
+        AgoraFeature.QagArchive -> "IS_QAG_ARCHIVE_ENABLED"
+    }
+
+    private fun getCache() = cacheManager.getCache(FEATURE_FLAGS_CACHE_NAME)
 
 }
--- src/main/kotlin/fr/social/gouv/agora/usecase/featureFlags/repository/FeatureFlagsRepository.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/featureFlags/repository/FeatureFlagsRepository.kt
@@ -1,7 +1,7 @@
 package fr.social.gouv.agora.usecase.featureFlags.repository
 
-import fr.social.gouv.agora.domain.FeatureFlags
+import fr.social.gouv.agora.domain.AgoraFeature
 
 interface FeatureFlagsRepository {
-    fun getFeatureFlags(): FeatureFlags
+    fun isFeatureEnabled(feature: AgoraFeature): Boolean
 }
