2c5b5828f6851e4949ffbffc0f9671643940df7b
imaneryane <imane.ryane@octo.com>
2023-09-13 15:57:27 +0200

    ajout controller, use case et mapper pour l'affichage paginé des notifications

---
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/notification/NotificationPaginatedController.kt
@@ -0,0 +1,31 @@
+package fr.social.gouv.agora.infrastructure.notification
+
+import fr.social.gouv.agora.security.jwt.JwtTokenUtils
+import fr.social.gouv.agora.usecase.notification.NotificationPaginatedUseCase
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@Suppress("unused")
+class NotificationPaginatedController(
+    private val notificationPaginatedUseCase: NotificationPaginatedUseCase,
+    private val notificationPaginatedJsonMapper: NotificationPaginatedJsonMapper,
+) {
+
+    @GetMapping("/notifications/paginated/{pageNumber}")
+    fun getNotificationList(
+        @RequestHeader("Authorization") authorizationHeader: String,
+        @PathVariable pageNumber: String,
+    ): ResponseEntity<*> {
+        val userId = JwtTokenUtils.extractUserIdFromHeader(authorizationHeader)
+        val usedPageNumber = pageNumber.toIntOrNull()
+
+        return usedPageNumber?.let {
+            notificationPaginatedUseCase.getNotificationPaginated(userId = userId, pageNumber = usedPageNumber)
+                ?.let { notificationListAndHasMoreNotificationsFlag ->
+                    ResponseEntity.ok(notificationPaginatedJsonMapper.toJson(notificationListAndHasMoreNotificationsFlag))
+                } ?: ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Unit)
+        } ?: ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Unit)
+    }
+}
