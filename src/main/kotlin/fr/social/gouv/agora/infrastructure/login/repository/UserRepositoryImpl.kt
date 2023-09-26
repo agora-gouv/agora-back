a4dd47a4774f56e2116db16866d0648d2bc37cd7
Thierry LEE <thierry.leet@gmail.com>
2023-09-26 14:22:44 +0200

    Refactor UserRepository

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/login/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/login/repository/UserRepositoryImpl.kt
@@ -15,14 +15,12 @@ class UserRepositoryImpl(
 ) : UserRepository {
 
     override fun getAllUsers(): List<UserInfo> {
-        return getAllUserDTO().map(mapper::toDomain)
+        return databaseRepository.findAll().map(mapper::toDomain)
     }
 
     override fun getUserById(userId: String): UserInfo? {
         return try {
-            val userUUID = UUID.fromString(userId)
-            getAllUserDTO()
-                .find { userDTO -> userDTO.id == userUUID }
+            getUserDTO(UUID.fromString(userId))
                 ?.let(mapper::toDomain)
         } catch (e: IllegalArgumentException) {
             null
@@ -32,19 +30,11 @@ class UserRepositoryImpl(
     @Suppress("UNCHECKED_CAST")
     override fun updateUser(userId: String, fcmToken: String): UserInfo? {
         return try {
-            val userUUID = UUID.fromString(userId)
-            val userDTO = findUserDTO(userUUID)
-            if (userDTO != null) {
+            getUserDTO(UUID.fromString(userId))?.let { userDTO ->
                 val updatedUserDTO = mapper.updateDto(dto = userDTO, fcmToken = fcmToken)
                 val savedUserDTO = databaseRepository.save(updatedUserDTO)
-                try {
-                    cacheRepository.updateUser(savedUserDTO)
-                } catch (e: IllegalStateException) {
-                    getAllUserFromDatabaseAndInitializeCache()
-                }
+                cacheRepository.updateUser(savedUserDTO)
                 mapper.toDomain(savedUserDTO)
-            } else {
-                null
             }
         } catch (e: IllegalArgumentException) {
             null
@@ -55,23 +45,21 @@ class UserRepositoryImpl(
     override fun generateUser(fcmToken: String): UserInfo {
         val userDTO = mapper.generateDto(fcmToken = fcmToken)
         val savedUserDTO = databaseRepository.save(userDTO)
-        try {
-            cacheRepository.insertUser(savedUserDTO)
-        } catch (e: IllegalStateException) {
-            getAllUserFromDatabaseAndInitializeCache()
-        }
+        cacheRepository.insertUser(savedUserDTO)
         return mapper.toDomain(savedUserDTO)
     }
 
-    private fun getAllUserDTO() = when (val cacheResult = cacheRepository.getAllUserList()) {
-        is CacheResult.CachedUserList -> cacheResult.allUserDTO
-        CacheResult.CacheNotInitialized -> getAllUserFromDatabaseAndInitializeCache()
+    private fun getUserDTO(userUUID: UUID): UserDTO? {
+        return when (val cacheResult = cacheRepository.getUser(userUUID)) {
+            is CacheResult.CachedUser -> cacheResult.userDTO
+            CacheResult.CacheUserNotFound -> null
+            CacheResult.CacheNotInitialized -> databaseRepository.getUserById(userUUID).also { userDTO ->
+                when (userDTO) {
+                    null -> cacheRepository.insertUserNotFound(userUUID)
+                    else -> cacheRepository.insertUser(userDTO)
+                }
+            }
+        }
     }
 
-    @Suppress("UNCHECKED_CAST")
-    private fun getAllUserFromDatabaseAndInitializeCache() = databaseRepository.findAll().also { allUserDTO ->
-        cacheRepository.initializeCache(allUserDTO as List<UserDTO>)
-    }
-
-    private fun findUserDTO(userUUID: UUID) = getAllUserDTO().find { userDTO -> userDTO.id == userUUID }
 }
