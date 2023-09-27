49374fd9aa5a7ed20779d1f2d86b65c100f47cb6
Thierry LEE <thierry.leet@gmail.com>
2023-09-27 18:30:30 +0200

    Simplify ConsultationPreviewPageRepository

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/consultation/repository/ConsultationPreviewPageRepositoryImpl.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/consultation/repository/ConsultationPreviewPageRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package fr.social.gouv.agora.infrastructure.consultation.repository
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
 import fr.social.gouv.agora.domain.*
 import fr.social.gouv.agora.usecase.consultation.repository.ConsultationPreviewPageRepository
 import org.springframework.cache.CacheManager
@@ -11,6 +11,7 @@ import java.util.*
 @Suppress("unused")
 class ConsultationPreviewPageRepositoryImpl(
     private val cacheManager: CacheManager,
+    private val objectMapper: ObjectMapper,
 ) : ConsultationPreviewPageRepository {
 
     companion object {
@@ -24,39 +25,33 @@ class ConsultationPreviewPageRepositoryImpl(
     @Suppress("UNCHECKED_CAST")
     override fun getConsultationPreviewOngoingList(userId: String): List<ConsultationPreviewOngoing>? {
         return try {
-            val modelList = getCache()?.get(
-                "$ONGOING_CACHE_PREFIX$userId",
-                List::class.java,
-            ) as? List<ConsultationPreviewOngoingModel>
-            return modelList?.map(::toOngoingPreview)
+            val modelList = getCache()?.get("$ONGOING_CACHE_PREFIX/$userId", List::class.java) as? List<String>
+            return modelList?.map { objectMapper.readValue(it, ConsultationPreviewOngoing::class.java) }
         } catch (e: IllegalStateException) {
             null
         }
     }
 
     override fun insertConsultationPreviewOngoingList(userId: String, ongoingList: List<ConsultationPreviewOngoing>) {
-        getCache()?.put("$ONGOING_CACHE_PREFIX$userId", ongoingList.map(::toModel))
+        getCache()?.put("$ONGOING_CACHE_PREFIX/$userId", ongoingList.map { objectMapper.writeValueAsString(it) })
     }
 
     override fun evictConsultationPreviewOngoingList(userId: String) {
-        getCache()?.evict("$ONGOING_CACHE_PREFIX$userId")
+        getCache()?.evict("$ONGOING_CACHE_PREFIX/$userId")
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun getConsultationPreviewFinishedList(): List<ConsultationPreviewFinished>? {
         return try {
-            val modelList = getCache()?.get(
-                FINISHED_CACHE_KEY,
-                List::class.java
-            ) as? List<ConsultationPreviewWithStatusModel>
-            return modelList?.map(::toFinishedPreview)
+            val modelList = getCache()?.get(FINISHED_CACHE_KEY, List::class.java) as? List<String>
+            return modelList?.map { objectMapper.readValue(it, ConsultationPreviewFinished::class.java) }
         } catch (e: IllegalStateException) {
             null
         }
     }
 
     override fun insertConsultationPreviewFinishedList(finishedList: List<ConsultationPreviewFinished>) {
-        getCache()?.put(FINISHED_CACHE_KEY, finishedList.map(::toModel))
+        getCache()?.put(FINISHED_CACHE_KEY, finishedList.map { objectMapper.writeValueAsString(it) })
     }
 
     override fun evictConsultationPreviewFinishedList() {
@@ -66,11 +61,8 @@ class ConsultationPreviewPageRepositoryImpl(
     @Suppress("UNCHECKED_CAST")
     override fun getConsultationPreviewAnsweredList(userId: String): List<ConsultationPreviewAnswered>? {
         return try {
-            val modelList = getCache()?.get(
-                ANSWERED_CACHE_PREFIX,
-                List::class.java
-            ) as? List<ConsultationPreviewWithStatusModel>
-            return modelList?.map(::toAnsweredPreview)
+            val modelList = getCache()?.get(ANSWERED_CACHE_PREFIX, List::class.java) as? List<String>
+            return modelList?.map { objectMapper.readValue(it, ConsultationPreviewAnswered::class.java) }
         } catch (e: IllegalStateException) {
             null
         }
@@ -80,7 +72,7 @@ class ConsultationPreviewPageRepositoryImpl(
         userId: String,
         answeredList: List<ConsultationPreviewAnswered>,
     ) {
-        getCache()?.put("$ANSWERED_CACHE_PREFIX$userId", answeredList.map(::toModel))
+        getCache()?.put("$ANSWERED_CACHE_PREFIX$userId", answeredList.map { objectMapper.writeValueAsString(it) })
     }
 
     override fun evictConsultationPreviewAnsweredList(userId: String) {
@@ -88,122 +80,4 @@ class ConsultationPreviewPageRepositoryImpl(
     }
 
     private fun getCache() = cacheManager.getCache(CONSULTATION_PREVIEW_PAGE_CACHE_NAME)
-
-    private fun toModel(consultation: ConsultationPreviewOngoing) = ConsultationPreviewOngoingModel(
-        id = consultation.id,
-        title = consultation.title,
-        coverUrl = consultation.coverUrl,
-        thematiqueId = consultation.thematique.id,
-        thematiqueLabel = consultation.thematique.label,
-        thematiquePicto = consultation.thematique.picto,
-        endDateTime = consultation.endDate.time,
-        highlightLabel = consultation.highlightLabel,
-    )
-
-    private fun toOngoingPreview(model: ConsultationPreviewOngoingModel) = ConsultationPreviewOngoing(
-        id = model.id,
-        title = model.title,
-        coverUrl = model.coverUrl,
-        thematique = Thematique(
-            id = model.thematiqueId,
-            label = model.thematiqueLabel,
-            picto = model.thematiquePicto,
-        ),
-        endDate = Date(model.endDateTime),
-        highlightLabel = model.highlightLabel,
-    )
-
-    private fun toModel(consultation: ConsultationPreviewFinished) = ConsultationPreviewWithStatusModel(
-        id = consultation.id,
-        title = consultation.title,
-        coverUrl = consultation.coverUrl,
-        thematiqueId = consultation.thematique.id,
-        thematiqueLabel = consultation.thematique.label,
-        thematiquePicto = consultation.thematique.picto,
-        step = toStepModel(consultation.step),
-    )
-
-    private fun toFinishedPreview(model: ConsultationPreviewWithStatusModel) = ConsultationPreviewFinished(
-        id = model.id,
-        title = model.title,
-        coverUrl = model.coverUrl,
-        thematique = Thematique(
-            id = model.thematiqueId,
-            label = model.thematiqueLabel,
-            picto = model.thematiquePicto,
-        ),
-        step = toStep(model.step),
-    )
-
-    private fun toModel(consultation: ConsultationPreviewAnswered) = ConsultationPreviewWithStatusModel(
-        id = consultation.id,
-        title = consultation.title,
-        coverUrl = consultation.coverUrl,
-        thematiqueId = consultation.thematique.id,
-        thematiqueLabel = consultation.thematique.label,
-        thematiquePicto = consultation.thematique.picto,
-        step = toStepModel(consultation.step),
-    )
-
-    private fun toAnsweredPreview(model: ConsultationPreviewWithStatusModel) = ConsultationPreviewAnswered(
-        id = model.id,
-        title = model.title,
-        coverUrl = model.coverUrl,
-        thematique = Thematique(
-            id = model.thematiqueId,
-            label = model.thematiqueLabel,
-            picto = model.thematiquePicto,
-        ),
-        step = toStep(model.step),
-    )
-
-    private fun toStepModel(consultationStatus: ConsultationStatus) = when (consultationStatus) {
-        ConsultationStatus.COLLECTING_DATA -> 1
-        ConsultationStatus.POLITICAL_COMMITMENT -> 2
-        ConsultationStatus.EXECUTION -> 3
-    }
-
-    private fun toStep(step: Int) = when (step) {
-        1 -> ConsultationStatus.COLLECTING_DATA
-        2 -> ConsultationStatus.POLITICAL_COMMITMENT
-        3 -> ConsultationStatus.EXECUTION
-        else -> throw IllegalStateException("Invalid ConsultationStatus: $step")
-    }
 }
-
-data class ConsultationPreviewOngoingModel(
-    @JsonProperty("id")
-    val id: String,
-    @JsonProperty("title")
-    val title: String,
-    @JsonProperty("coverUrl")
-    val coverUrl: String,
-    @JsonProperty("thematiqueId")
-    val thematiqueId: String,
-    @JsonProperty("thematiqueLabel")
-    val thematiqueLabel: String,
-    @JsonProperty("thematiquePicto")
-    val thematiquePicto: String,
-    @JsonProperty("endDateLong")
-    val endDateTime: Long,
-    @JsonProperty("highlightLabel")
-    val highlightLabel: String?,
-)
-
-
-data class ConsultationPreviewWithStatusModel(
-    @JsonProperty("id")
-    val id: String,
-    @JsonProperty("title")
-    val title: String,
-    @JsonProperty("coverUrl")
-    val coverUrl: String,
-    @JsonProperty("thematiqueId")
-    val thematiqueId: String,
-    @JsonProperty("thematiqueLabel")
-    val thematiqueLabel: String,
-    @JsonProperty("thematiquePicto")
-    val thematiquePicto: String,
-    @JsonProperty("step")
-    val step: Int,
-)
