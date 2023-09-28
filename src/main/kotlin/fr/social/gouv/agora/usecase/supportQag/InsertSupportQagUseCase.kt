1703e4ff265dd4d169379818f7a365054787f250
Thierry LEE <thierry.leet@gmail.com>
2023-09-28 11:04:09 +0200

    Add logs on add/remove support on QaG

--- src/main/kotlin/fr/social/gouv/agora/usecase/supportQag/InsertSupportQagUseCase.kt
+++ src/main/kotlin/fr/social/gouv/agora/usecase/supportQag/InsertSupportQagUseCase.kt
@@ -5,7 +5,6 @@ import fr.social.gouv.agora.domain.SupportQagInserting
 import fr.social.gouv.agora.usecase.qag.repository.QagInfo
 import fr.social.gouv.agora.usecase.qag.repository.QagInfoRepository
 import fr.social.gouv.agora.usecase.qagPreview.repository.QagPreviewPageRepository
-import fr.social.gouv.agora.usecase.responseQag.repository.ResponseQagRepository
 import fr.social.gouv.agora.usecase.supportQag.repository.GetSupportQagRepository
 import fr.social.gouv.agora.usecase.supportQag.repository.SupportQagRepository
 import fr.social.gouv.agora.usecase.supportQag.repository.SupportQagResult
@@ -13,27 +12,33 @@ import org.springframework.stereotype.Service
 
 @Service
 class InsertSupportQagUseCase(
-    private val responseQagRepository: ResponseQagRepository,
     private val qagInfoRepository: QagInfoRepository,
     private val getSupportQagRepository: GetSupportQagRepository,
     private val supportQagRepository: SupportQagRepository,
     private val previewPageRepository: QagPreviewPageRepository,
 ) {
     fun insertSupportQag(supportQagInserting: SupportQagInserting): SupportQagResult {
-        if (responseQagRepository.getResponseQag(supportQagInserting.qagId) != null) return SupportQagResult.FAILURE
         if (getSupportQagRepository.getSupportQag(
                 qagId = supportQagInserting.qagId,
                 userId = supportQagInserting.userId,
             )?.isSupportedByUser == true
-        ) return SupportQagResult.FAILURE
+        ) {
+            println("⚠️ Add support error: already supported")
+            return SupportQagResult.FAILURE
+        }
 
         val qagInfo = qagInfoRepository.getQagInfo(supportQagInserting.qagId)
         return when (qagInfo?.status) {
-            null, QagStatus.ARCHIVED, QagStatus.MODERATED_REJECTED, QagStatus.SELECTED_FOR_RESPONSE -> SupportQagResult.FAILURE
+            null, QagStatus.ARCHIVED, QagStatus.MODERATED_REJECTED, QagStatus.SELECTED_FOR_RESPONSE -> {
+                println("⚠️ Add support error: QaG with invalid status")
+                SupportQagResult.FAILURE
+            }
             QagStatus.OPEN, QagStatus.MODERATED_ACCEPTED -> {
                 val supportResult = supportQagRepository.insertSupportQag(supportQagInserting)
                 if (supportResult == SupportQagResult.SUCCESS) {
                     updateCache(qagInfo, supportQagInserting.userId)
+                } else {
+                    println("⚠️ Add support error")
                 }
                 supportResult
             }
