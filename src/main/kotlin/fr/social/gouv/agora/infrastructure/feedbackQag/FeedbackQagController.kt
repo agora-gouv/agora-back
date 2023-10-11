00578673826fc8ecd20ffef15a4b71ad70e89329
imaneryane <imane.ryane@octo.com>
2023-10-11 18:01:45 +0200

    ajout système de queue pour l'insertion des feedbacks sur les reponses aux Qag

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/feedbackQag/FeedbackQagController.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/feedbackQag/FeedbackQagController.kt
@@ -10,23 +10,32 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @Suppress("unused")
-class FeedbackQagController(private val insertFeedbackQagUseCase: InsertFeedbackQagUseCase) {
-
+class FeedbackQagController(
+    private val insertFeedbackQagUseCase: InsertFeedbackQagUseCase,
+    private val queue: FeedbackQagQueue,
+) {
     @PostMapping("/qags/{qagId}/feedback")
     fun insertFeedbackQag(
         @RequestHeader("Authorization") authorizationHeader: String,
         @PathVariable qagId: String,
         @RequestBody body: FeedbackQagJson,
     ): HttpEntity<*> {
-        val insertResult = insertFeedbackQagUseCase.insertFeedbackQag(
-            FeedbackQagInserting(
-                qagId = qagId,
-                userId = JwtTokenUtils.extractUserIdFromHeader(authorizationHeader),
-                isHelpful = body.isHelpful,
-            )
+        val userId = JwtTokenUtils.extractUserIdFromHeader(authorizationHeader)
+        return queue.executeTask(
+            taskType = FeedbackQagQueue.TaskType.AddFeedback(userId = userId),
+            onTaskExecuted = {
+                val insertResult = insertFeedbackQagUseCase.insertFeedbackQag(
+                    FeedbackQagInserting(
+                        qagId = qagId,
+                        userId = userId,
+                        isHelpful = body.isHelpful,
+                    )
+                )
+                if (insertResult == FeedbackQagResult.SUCCESS) {
+                    ResponseEntity.ok().body("")
+                } else ResponseEntity.status(400).body("")
+            },
+            onTaskRejected = { ResponseEntity.badRequest().body(Unit) }
         )
-        return if (insertResult == FeedbackQagResult.SUCCESS) {
-            ResponseEntity.ok().body("")
-        } else ResponseEntity.status(400).body("")
     }
 }
