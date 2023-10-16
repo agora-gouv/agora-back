49687e067a598973c93f3a5d2a326b57bef61c41
Thierry LEE <thierry.leet@gmail.com>
2023-10-16 14:44:31 +0200

    [TECH] Put feature flags in eternal cache (#294)

--- src/main/kotlin/fr/social/gouv/agora/infrastructure/featureFlags/repository/FeatureFlagsRepositoryImpl.kt
+++ src/main/kotlin/fr/social/gouv/agora/infrastructure/featureFlags/repository/FeatureFlagsRepositoryImpl.kt
@@ -2,12 +2,15 @@ package fr.social.gouv.agora.infrastructure.featureFlags.repository
 
 import fr.social.gouv.agora.domain.AgoraFeature
 import fr.social.gouv.agora.usecase.featureFlags.repository.FeatureFlagsRepository
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.cache.CacheManager
 import org.springframework.stereotype.Component
 
 @Component
 @Suppress("unused")
-class FeatureFlagsRepositoryImpl(private val cacheManager: CacheManager) : FeatureFlagsRepository {
+class FeatureFlagsRepositoryImpl(
+    @Qualifier("eternalCacheManager") private val cacheManager: CacheManager,
+) : FeatureFlagsRepository {
 
     companion object {
         private const val FEATURE_FLAGS_CACHE_NAME = "featureFlags"
