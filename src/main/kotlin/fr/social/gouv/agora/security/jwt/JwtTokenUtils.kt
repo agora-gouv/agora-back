45247be3dfca530e8b871b1fc4d2ba996d374008
Thierry LEE <thierry.leet@gmail.com>
2023-10-16 13:52:47 +0200

    Remove short validity JWT

--- src/main/kotlin/fr/social/gouv/agora/security/jwt/JwtTokenUtils.kt
+++ src/main/kotlin/fr/social/gouv/agora/security/jwt/JwtTokenUtils.kt
@@ -7,22 +7,13 @@ import io.jsonwebtoken.security.Keys
 import java.util.*
 import java.util.concurrent.TimeUnit
 
-
 object JwtTokenUtils {
 
     private val JWT_TOKEN_VALIDITY = TimeUnit.DAYS.toMillis(1)
     private const val JWT_PREFIX = "Bearer "
 
-    private val SHORT_JWT_TOKEN_VALIDITY = TimeUnit.SECONDS.toMillis(15)
-    private const val SHORT_JWT_TOKEN_USER_ID = "b6953c78-dec3-4fa6-8d9f-18dd44271874"
-
     fun generateToken(userId: String, claims: Map<String, Any> = emptyMap()): Pair<String, Long> {
-        val expirationDate = if (userId == SHORT_JWT_TOKEN_USER_ID) {
-            Date(System.currentTimeMillis() + SHORT_JWT_TOKEN_VALIDITY)
-        } else {
-            Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY)
-        }
-
+        val expirationDate = Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY)
         return Jwts.builder()
             .setSubject(userId)
             .setIssuedAt(Date())
