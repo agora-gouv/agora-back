044df0e969472a40b307303dc58975e119cc2e61
Thierry LEE <thierry.leet@gmail.com>
2023-10-31 14:35:43 +0100

    Optimize consultation results & put it back as an admin API

--- src/main/kotlin/fr/gouv/agora/domain/ReponseConsultation.kt
+++ src/main/kotlin/fr/gouv/agora/domain/ReponseConsultation.kt
@@ -15,6 +15,20 @@ data class ResponseConsultationCount(
     val responseCount: Int,
 )
 
+data class ResponseConsultationCountWithDemographicInfo(
+    val questionId: String,
+    val choiceId: String,
+    val responseCount: Int,
+    val gender: Gender?,
+    val yearOfBirth: Int?,
+    val department: Department?,
+    val cityType: CityType?,
+    val jobCategory: JobCategory?,
+    val voteFrequency: Frequency?,
+    val publicMeetingFrequency: Frequency?,
+    val consultationFrequency: Frequency?,
+)
+
 data class ReponseConsultationInserting(
     val questionId: String,
     val choiceIds: List<String>?,
