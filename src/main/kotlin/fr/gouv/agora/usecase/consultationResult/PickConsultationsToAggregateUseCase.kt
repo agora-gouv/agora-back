d8b408365cc25f9a96375a567a5ea68ac48f1f3b
Thierry LEE <thierry.leet@gmail.com>
2024-01-08 16:06:52 +0100

    Minor optimizations

--- src/main/kotlin/fr/gouv/agora/usecase/consultationResult/PickConsultationsToAggregateUseCase.kt
+++ src/main/kotlin/fr/gouv/agora/usecase/consultationResult/PickConsultationsToAggregateUseCase.kt
@@ -1,35 +1,18 @@
 package fr.gouv.agora.usecase.consultationResult
 
-import fr.gouv.agora.infrastructure.utils.DateUtils.toLocalDate
 import fr.gouv.agora.usecase.consultation.repository.ConsultationInfoRepository
 import fr.gouv.agora.usecase.reponseConsultation.repository.ConsultationResultAggregatedRepository
 import org.springframework.stereotype.Component
-import java.time.Clock
-import java.time.LocalDate
 
 @Component
 class PickConsultationsToAggregateUseCase(
-    private val clock: Clock,
     private val consultationInfoRepository: ConsultationInfoRepository,
     private val consultationAggregatedRepository: ConsultationResultAggregatedRepository,
     private val aggregateConsultationResultUseCase: AggregateConsultationResultUseCase,
 ) {
 
-    companion object {
-        private const val MINIMUM_DAYS_AFTER_CONSULTATION_END_TO_AGGREGATE = 14L
-    }
-
     fun aggregateConsultations() {
-        val currentDate = LocalDate.now(clock)
-        consultationInfoRepository.getConsultations()
-            .filter { consultationInfo ->
-                currentDate.isAfter(
-                    consultationInfo
-                        .endDate
-                        .toLocalDate()
-                        .plusDays(MINIMUM_DAYS_AFTER_CONSULTATION_END_TO_AGGREGATE)
-                )
-            }
+        consultationInfoRepository.getConsultationsToAggregate()
             .takeIf { it.isNotEmpty() }
             ?.let { consultationInfoList ->
                 val aggregatedConsultationIds = consultationAggregatedRepository.getAggregatedResultsConsultationIds()
--- src/test/kotlin/fr/gouv/agora/usecase/consultationResult/PickConsultationsToAggregateUseCaseTest.kt
+++ src/test/kotlin/fr/gouv/agora/usecase/consultationResult/PickConsultationsToAggregateUseCaseTest.kt
@@ -1,23 +1,21 @@
 package fr.gouv.agora.usecase.consultationResult
 
-import fr.gouv.agora.TestUtils
-import fr.gouv.agora.infrastructure.utils.DateUtils.toDate
 import fr.gouv.agora.usecase.consultation.repository.ConsultationInfo
 import fr.gouv.agora.usecase.consultation.repository.ConsultationInfoRepository
 import fr.gouv.agora.usecase.reponseConsultation.repository.ConsultationResultAggregatedRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.*
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import java.time.LocalDate
-import java.time.Month
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
 class PickConsultationsToAggregateUseCaseTest {
 
+    @Autowired
     private lateinit var useCase: PickConsultationsToAggregateUseCase
 
     @MockBean
@@ -32,91 +30,55 @@ class PickConsultationsToAggregateUseCaseTest {
     @Test
     fun `aggregateConsultations - when no consultations - should do nothing`() {
         // Given
-        mockCurrentDate()
-        given(consultationInfoRepository.getConsultations()).willReturn(emptyList())
+        given(consultationInfoRepository.getConsultationsToAggregate()).willReturn(emptyList())
 
         // When
         useCase.aggregateConsultations()
 
         // Then
-        then(consultationInfoRepository).should(only()).getConsultations()
+        then(consultationInfoRepository).should(only()).getConsultationsToAggregate()
         then(consultationAggregatedRepository).shouldHaveNoInteractions()
         then(aggregateConsultationResultUseCase).shouldHaveNoInteractions()
     }
 
     @Test
-    fun `aggregateConsultations - when has consultations that aren't finished or finished but for less than 15 days - should do nothing`() {
+    fun `aggregateConsultations - when has consultations that are not already aggregated - should aggregate them`() {
         // Given
-        mockCurrentDate(currentDate = LocalDate.of(1998, Month.JULY, 15))
         val consultations = listOf(
-            mockConsultation(id = "consultationId1", endDate = LocalDate.of(1998, Month.SEPTEMBER, 21)),
-            mockConsultation(id = "consultationId2", endDate = LocalDate.of(1998, Month.JULY, 16)),
+            mock(ConsultationInfo::class.java).also {
+                given(it.id).willReturn("consultationId")
+            }
         )
-        given(consultationInfoRepository.getConsultations()).willReturn(consultations)
+        given(consultationInfoRepository.getConsultationsToAggregate()).willReturn(consultations)
 
         // When
         useCase.aggregateConsultations()
 
         // Then
-        then(consultationInfoRepository).should(only()).getConsultations()
-        then(consultationAggregatedRepository).shouldHaveNoInteractions()
-        then(aggregateConsultationResultUseCase).shouldHaveNoInteractions()
-    }
-
-    @Test
-    fun `aggregateConsultations - when has consultations that are finished for more than 15 days and not already aggregated - should aggregate them`() {
-        // Given
-        mockCurrentDate(currentDate = LocalDate.of(1998, Month.JULY, 15))
-        val consultations = listOf(
-            mockConsultation(id = "consultationId0", endDate = LocalDate.of(1998, Month.JULY, 2)),
-            mockConsultation(id = "consultationId1", endDate = LocalDate.of(1998, Month.JULY, 1)),
-            mockConsultation(id = "consultationId2", endDate = LocalDate.of(1998, Month.JUNE, 30)),
-        )
-        given(consultationInfoRepository.getConsultations()).willReturn(consultations)
-
-        // When
-        useCase.aggregateConsultations()
-
-        // Then
-        then(consultationInfoRepository).should(only()).getConsultations()
+        then(consultationInfoRepository).should(only()).getConsultationsToAggregate()
         then(consultationAggregatedRepository).should(only()).getAggregatedResultsConsultationIds()
-        then(aggregateConsultationResultUseCase).should(only()).aggregateConsultationResults("consultationId2")
+        then(aggregateConsultationResultUseCase).should(only()).aggregateConsultationResults("consultationId")
     }
 
     @Test
-    fun `aggregateConsultations - when has consultations that are finished for more than 15 days but already aggregated - should do nothing`() {
+    fun `aggregateConsultations - when has consultations that already aggregated - should do nothing`() {
         // Given
-        mockCurrentDate(currentDate = LocalDate.of(1998, Month.JULY, 15))
         val consultations = listOf(
-            mockConsultation(id = "consultationId2", endDate = LocalDate.of(1998, Month.JUNE, 30)),
+            mock(ConsultationInfo::class.java).also {
+                given(it.id).willReturn("consultationId")
+            }
         )
-        given(consultationInfoRepository.getConsultations()).willReturn(consultations)
+        given(consultationInfoRepository.getConsultationsToAggregate()).willReturn(consultations)
         given(consultationAggregatedRepository.getAggregatedResultsConsultationIds())
-            .willReturn(listOf("consultationId2"))
+            .willReturn(listOf("consultationId"))
 
         // When
         useCase.aggregateConsultations()
 
         // Then
-        then(consultationInfoRepository).should(only()).getConsultations()
+        then(consultationInfoRepository).should(only()).getConsultationsToAggregate()
         then(consultationAggregatedRepository).should(only()).getAggregatedResultsConsultationIds()
         then(aggregateConsultationResultUseCase).shouldHaveNoInteractions()
-    }
-
-    private fun mockCurrentDate(currentDate: LocalDate = LocalDate.MIN) {
-        useCase = PickConsultationsToAggregateUseCase(
-            clock = TestUtils.getFixedClock(currentDate.atStartOfDay()),
-            consultationInfoRepository = consultationInfoRepository,
-            consultationAggregatedRepository = consultationAggregatedRepository,
-            aggregateConsultationResultUseCase = aggregateConsultationResultUseCase,
-        )
-    }
-
-    private fun mockConsultation(id: String, endDate: LocalDate): ConsultationInfo {
-        return mock(ConsultationInfo::class.java).also {
-            given(it.id).willReturn(id)
-            given(it.endDate).willReturn(endDate.toDate())
-        }
     }
 
 }
