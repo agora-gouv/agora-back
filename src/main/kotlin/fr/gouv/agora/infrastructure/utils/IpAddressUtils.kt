a44855d26556966a895682203fdcd8d2833bb4ee
Thierry LEE <thierry.leet@gmail.com>
2024-02-14 17:30:53 +0100

    Fix IpAddressUtils on local servers

--- src/main/kotlin/fr/gouv/agora/infrastructure/utils/IpAddressUtils.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/utils/IpAddressUtils.kt
@@ -6,9 +6,9 @@ object IpAddressUtils {
 
     fun retrieveIpAddress(request: HttpServletRequest): String {
         return (
-                request.getHeader("X-Forwarded-For").takeIf { it.isNotBlank() }
+                request.getHeader("X-Forwarded-For")?.takeIf { it.isNotBlank() }
                     ?.let { addresses -> addresses.split(",").firstOrNull { address -> address.trim().isNotBlank() } }
-                    ?: request.getHeader("X-Remote-Address").takeIf { it.isNotBlank() }
+                    ?: request.getHeader("X-Remote-Address")?.takeIf { it.isNotBlank() }
                     ?: request.remoteAddr
                 ).trim()
     }
