e9f626369712d8288bdc0359430e259d1d32850f
ENZO <79970665+encalv@users.noreply.github.com>
2024-04-30 10:08:28 +0200

    [FIX] Correction wording (= mardi) sur l'écran de question déjà posée (#435)

--- src/main/kotlin/fr/gouv/agora/infrastructure/errorMessages/repository/ErrorMessagesRepositoryImpl.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/errorMessages/repository/ErrorMessagesRepositoryImpl.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component
 class ErrorMessagesRepositoryImpl : ErrorMessagesRepository {
     companion object {
         private const val ERROR_TEXT_WITHIN_THE_WEEK =
-            "Vous avez déjà posé une question au Gouvernement cette semaine. L’appli propose actuellement une question par semaine pour chaque utilisateur afin que le plus grand nombre de citoyens puisse participer. Rendez-vous mardi à partir de 14h pour poser une nouvelle question. D’ici là, n’hésitez pas à soutenir les questions des autres utilisateurs, sans limite !"
+            "Vous avez déjà posé une question au Gouvernement cette semaine. L’appli propose actuellement une question par semaine pour chaque utilisateur afin que le plus grand nombre de citoyens puisse participer. Rendez-vous lundi à partir de 14h pour poser une nouvelle question. D’ici là, n’hésitez pas à soutenir les questions des autres utilisateurs, sans limite !"
     }
 
     override fun getQagDisabledErrorMessage(): String {
