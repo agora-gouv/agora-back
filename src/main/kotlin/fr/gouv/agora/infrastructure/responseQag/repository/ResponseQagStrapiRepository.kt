5352a68c617186a83e580e59fe40938cfb093cec
ATMA <pheno62@gmail.com>
2024-05-17 18:12:50 +0200

    format

--- src/main/kotlin/fr/gouv/agora/infrastructure/responseQag/repository/ResponseQagStrapiRepository.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/responseQag/repository/ResponseQagStrapiRepository.kt
@@ -1,16 +1,11 @@
 package fr.gouv.agora.infrastructure.responseQag.repository
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import fr.gouv.agora.config.CmsStrapiHttpClient
 import fr.gouv.agora.domain.ResponseQag
-import fr.gouv.agora.domain.ResponseQagText
 import fr.gouv.agora.infrastructure.responseQag.dto.StrapiResponseDTO
-import fr.gouv.agora.infrastructure.utils.DateUtils.toDate
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import java.time.LocalDate
 import java.util.UUID
 
 @Repository
@@ -24,7 +19,6 @@ class ResponseQagStrapiRepository(
             .getByIds("reponse-du-gouvernements", "questionId", qagIds.map { it.toString() })
 
         val responseQagFromStrapi = objectMapper
-            .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
             .readValue(allResponses, StrapiResponseDTO::class.java)
 
         return responseQagMapper.toDomain(responseQagFromStrapi)
