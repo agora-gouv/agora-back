999c8ca464cba2c7afc916ae9998f706135e8c10
Thierry LEE <thierry.leet@gmail.com>
2024-05-17 18:56:21 +0200

    Move suspicious activity detect to dedicated use case

--- src/main/kotlin/fr/gouv/agora/oninit/DailyTasksHandler.kt
+++ src/main/kotlin/fr/gouv/agora/oninit/DailyTasksHandler.kt
@@ -2,20 +2,20 @@ package fr.gouv.agora.oninit
 
 import fr.gouv.agora.usecase.consultation.ConsultationCacheClearUseCase
 import fr.gouv.agora.usecase.consultationAggregate.PickConsultationsToAggregateUseCase
-import fr.gouv.agora.usecase.suspiciousUser.IsSuspiciousUserUseCase
+import fr.gouv.agora.usecase.suspiciousUser.SuspiciousActivityDetectUseCase
 import org.springframework.stereotype.Component
 
 @Component
 class DailyTasksHandler(
     private val consultationCacheClearUseCase: ConsultationCacheClearUseCase,
     private val pickConsultationsToAggregateUseCase: PickConsultationsToAggregateUseCase,
-    private val suspiciousUserUseCase: IsSuspiciousUserUseCase,
+    private val suspiciousActivityDetectUseCase: SuspiciousActivityDetectUseCase,
 ) : CustomCommandHandler {
 
     override fun handleTask(arguments: Map<String, String>?) {
         consultationCacheClearUseCase.clearConsultationCaches()
         pickConsultationsToAggregateUseCase.aggregateConsultations()
-        suspiciousUserUseCase.flagSuspiciousUsers()
+        suspiciousActivityDetectUseCase.flagSuspiciousUsers()
         // TODOs
         // - Delete everything related to a user when last connection date is over 2 years (except QaG if status is SELECTED_FOR_RESPONSE)
         // - Remove feedback link to userId ?
--- src/main/kotlin/fr/gouv/agora/usecase/suspiciousUser/IsSuspiciousUserUseCase.kt
+++ src/main/kotlin/fr/gouv/agora/usecase/suspiciousUser/IsSuspiciousUserUseCase.kt
@@ -2,11 +2,9 @@ package fr.gouv.agora.usecase.suspiciousUser
 
 import fr.gouv.agora.domain.AgoraFeature
 import fr.gouv.agora.domain.SignupHistoryCount
-import fr.gouv.agora.infrastructure.utils.DateUtils.toDate
 import fr.gouv.agora.usecase.featureFlags.repository.FeatureFlagsRepository
 import fr.gouv.agora.usecase.login.repository.UserDataRepository
 import fr.gouv.agora.usecase.suspiciousUser.repository.SignupCountRepository
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.time.Clock
 import java.time.LocalDate
@@ -22,8 +20,6 @@ class IsSuspiciousUserUseCase(
     companion object {
         private const val SOFT_BAN_SIGNUP_COUNT = 10
     }
-
-    private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun isSuspiciousActivity(ipAddressHash: String, userAgent: String): Boolean {
         if (!featureFlagsRepository.isFeatureEnabled(AgoraFeature.SuspiciousUserDetection)) return false
@@ -43,20 +39,6 @@ class IsSuspiciousUserUseCase(
                 todaySignupCount = signupCount + 1,
             )
         }
-    }
-
-    fun flagSuspiciousUsers() {
-        if (!featureFlagsRepository.isFeatureEnabled(AgoraFeature.SuspiciousUserDetection)) return
-        logger.info("🏴 Flag users with suspicious activity")
-        val dateNow = LocalDate.now(clock)
-        val dateYesterday = dateNow.minusDays(1)
-
-        val suspiciousUsersFlaggedCount = userDataRepository.flagUsersWithSuspiciousActivity(
-            SOFT_BAN_SIGNUP_COUNT,
-            dateYesterday.toDate(),
-            dateNow.toDate(),
-        )
-        logger.info("🏴 $suspiciousUsersFlaggedCount users has been flagged for suspicious activity")
     }
 
     private fun buildTodaySignupCount(ipAddressHash: String, userAgent: String): Int {
