594635cca71d0dbfa888c82fe95141e05e7873c0
ATMA <pheno62@gmail.com>
2024-06-03 15:02:50 +0200

    tests

---
+++ src/main/kotlin/fr/gouv/agora/domain/News.kt
@@ -0,0 +1,4 @@
+package fr.gouv.agora.domain
+
+class News {
+}
--- src/main/kotlin/fr/gouv/agora/infrastructure/welcomePage/WelcomePageController.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/welcomePage/WelcomePageController.kt
@@ -1,32 +1,18 @@
 package fr.gouv.agora.infrastructure.welcomePage
 
-import fr.gouv.agora.infrastructure.welcomePage.repository.NewsDatabaseRepository
+import fr.gouv.agora.usecase.welcomePage.GetLastNewsUseCase
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
 
 @RestController
 @RequestMapping("/welcome_page")
 class WelcomePageController(
-    val newsDatabaseRepository: NewsDatabaseRepository
+    val getLastNewsUseCase: GetLastNewsUseCase,
 ) {
     @GetMapping("/last_news")
-    fun getThematiqueList(): ResponseEntity<NewsJson> {
-        val lastNews = newsDatabaseRepository
-            .getNewsList()
-            .filter { it.beginDate <= LocalDate.now() }
-            .maxBy { it.beginDate }
-
-        return ResponseEntity.ok()
-            .body(
-                NewsJson(
-                    lastNews.description,
-                    lastNews.callToActionText,
-                    lastNews.routeName,
-                    lastNews.routeArgument,
-                )
-            )
+    fun getLastNews(): ResponseEntity<NewsJson> {
+        return ResponseEntity.ok().body(getLastNewsUseCase.execute())
     }
 }
