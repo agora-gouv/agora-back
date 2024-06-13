ad2ed8e7dbe8750b05383f56068078defbc15433
ATMA <pheno62@gmail.com>
2024-06-13 17:27:44 +0200

    feat : ajout rich text

--- src/main/kotlin/fr/gouv/agora/infrastructure/responseQag/dto/StrapiResponseQagDTO.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/responseQag/dto/StrapiResponseQagDTO.kt
@@ -51,23 +51,73 @@ data class StrapiResponseQagVideo(
     val informationAdditionnelleDescription: List<StrapiRichText>?,
 ) : StrapiResponseQagType
 
-data class StrapiRichText(
+// Pour définir les types,
+// je me suis inspiré du thread https://forum.strapi.io/t/blocks-rich-text-editor/32588/5 sur le forum Strapi
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type", visible = true)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = StrapiRichTextNode::class, name = "text"),
+    JsonSubTypes.Type(value = StrapiRichLinkNode::class, name = "link"),
+    JsonSubTypes.Type(value = StrapiRichListItemNode::class, name = "list-item"),
+    JsonSubTypes.Type(value = StrapiRichBlockNode::class, name = "heading"),
+    JsonSubTypes.Type(value = StrapiRichBlockNode::class, name = "paragraph"),
+    JsonSubTypes.Type(value = StrapiRichBlockNode::class, name = "list"),
+    JsonSubTypes.Type(value = StrapiRichBlockNode::class, name = "quote"),
+)
+sealed interface StrapiRichText
+
+//data class StrapiRichText(
+//    @JsonProperty("text")
+//    val text: String?,
+//    @JsonProperty("type")
+//    val type: String,
+//    @JsonProperty("bold")
+//    val bold: Boolean?,
+//    @JsonProperty("underline")
+//    val underline: Boolean?,
+//    @JsonProperty("strikethrough")
+//    val strikethrough: Boolean?,
+//    @JsonProperty("children")
+//    val children: List<StrapiRichText>?,
+//    @JsonProperty("format")
+//    val format: String?,
+//    @JsonProperty("code")
+//    val code: Boolean?,
+//    @JsonProperty("level")
+//    val level: Int?,
+//)
+
+// todo : soit j'utilise un type générique mais je dois checker les champs
+// todo : soit j'utilise plusieurs types, que je peux dispatcher via le champs type (ça me permettrait de mieux gérer tous les cas)
+data class StrapiRichTextNode(
     @JsonProperty("text")
-    val text: String?,
-    @JsonProperty("type")
-    val type: String,
+    val text: String,
     @JsonProperty("bold")
     val bold: Boolean?,
     @JsonProperty("underline")
     val underline: Boolean?,
+    @JsonProperty("italic")
+    val italic: Boolean?,
     @JsonProperty("strikethrough")
     val strikethrough: Boolean?,
-    @JsonProperty("children")
-    val children: List<StrapiRichText>?,
-    @JsonProperty("format")
-    val format: String?,
     @JsonProperty("code")
     val code: Boolean?,
-    @JsonProperty("level")
+)
+data class StrapiRichLinkNode(
+    @JsonProperty("url")
+    val url: String,
+    @JsonProperty("children")
+    val children: List<StrapiRichTextNode>,
+)
+
+data class StrapiRichListItemNode(
+    @JsonProperty("children")
+    val children: List<StrapiRichText>, // en vrai on ne peut avoir que RichText ou RichLink
+)
+data class StrapiRichBlockNode(
+    @JsonProperty("let pvel")
     val level: Int?,
+    @JsonProperty("format")
+    val format: String?, // 'unordered' | 'ordered'
+    @JsonProperty("children")
+    val children: List<StrapiRichText>, // TextNode | LinkNode | ListItemNode
 )
