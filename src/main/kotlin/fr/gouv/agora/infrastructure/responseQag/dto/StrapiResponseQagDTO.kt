1af896abd5f77638636b0cbf2a3460c3010fd589
ATMA <pheno62@gmail.com>
2024-06-13 17:56:14 +0200

    add types

--- src/main/kotlin/fr/gouv/agora/infrastructure/responseQag/dto/StrapiResponseQagDTO.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/responseQag/dto/StrapiResponseQagDTO.kt
@@ -3,6 +3,8 @@ package fr.gouv.agora.infrastructure.responseQag.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.time.LocalDate
 
 data class StrapiResponseQag(
@@ -20,7 +22,12 @@ data class StrapiResponseQag(
     val reponseType: List<StrapiResponseQagType>
 )
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "__component", visible = true)
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.EXISTING_PROPERTY,
+    property = "__component",
+    visible = true
+)
 @JsonSubTypes(
     JsonSubTypes.Type(value = StrapiResponseQagText::class, name = "reponse.reponsetextuelle"),
     JsonSubTypes.Type(value = StrapiResponseQagVideo::class, name = "reponse.reponse-video")
@@ -54,17 +61,29 @@ data class StrapiResponseQagVideo(
 // Pour définir les types,
 // je me suis inspiré du thread https://forum.strapi.io/t/blocks-rich-text-editor/32588/5 sur le forum Strapi
 // et de la lib React https://github.com/strapi/blocks-react-renderer/
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type", visible = true)
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.EXISTING_PROPERTY,
+    property = "type",
+    visible = true
+)
 @JsonSubTypes(
     JsonSubTypes.Type(value = StrapiRichTextNode::class, name = "text"),
     JsonSubTypes.Type(value = StrapiRichLinkNode::class, name = "link"),
     JsonSubTypes.Type(value = StrapiRichListItemNode::class, name = "list-item"),
-    JsonSubTypes.Type(value = StrapiRichBlockNode::class, names = ["heading", "paragraph", "list", "quote"]),
+    JsonSubTypes.Type(value = StrapiRichHeadingNode::class, name = "heading"),
+    JsonSubTypes.Type(value = StrapiRichParagraphNode::class, name = "paragraph"),
+    JsonSubTypes.Type(value = StrapiRichListNode::class, name = "list"),
+    JsonSubTypes.Type(value = StrapiRichQuoteNode::class, name = "quote"),
 )
-sealed interface StrapiRichText
+sealed interface StrapiRichText {
+    fun toHtml(): String
 
-// todo : soit j'utilise un type générique mais je dois checker les champs
-// todo : soit j'utilise plusieurs types, que je peux dispatcher via le champs type (ça me permettrait de mieux gérer tous les cas)
+    fun List<StrapiRichText>.toHtml(): String {
+        return this.joinToString("") { it.toHtml() }
+    }
+}
+
 data class StrapiRichTextNode(
     @JsonProperty("text")
     val text: String,
@@ -78,25 +97,101 @@ data class StrapiRichTextNode(
     val strikethrough: Boolean?,
     @JsonProperty("code")
     val code: Boolean?,
-): StrapiRichText
+) : StrapiRichText {
+    override fun toHtml(): String {
+        var htmlText = text
+        if (bold != null && bold) {
+            htmlText = "<b>$htmlText</b>"
+        }
+        if (italic != null && italic) {
+            htmlText = "<em>$htmlText</em>"
+        }
+        if (underline != null && underline) {
+            htmlText = "<u>$htmlText</u>"
+        }
+        if (strikethrough != null && strikethrough) {
+            htmlText = "<del>$htmlText</del>"
+        }
+        if (code != null && code) {
+            htmlText = "<code>$htmlText</code>"
+        }
+        return htmlText
+    }
+}
 
 data class StrapiRichLinkNode(
     @JsonProperty("url")
     val url: String,
     @JsonProperty("children")
     val children: List<StrapiRichTextNode>,
-): StrapiRichText
+) : StrapiRichText {
+    override fun toHtml(): String {
+        return "<a href=\"$url\">${children.toHtml()}</a>"
+    }
+}
 
 data class StrapiRichListItemNode(
     @JsonProperty("children")
-    val children: List<StrapiRichText>, // en vrai on ne peut avoir que RichText ou RichLink
-): StrapiRichText
+    val children: List<StrapiRichText>, // RichText | RichLink
+) : StrapiRichText {
+    override fun toHtml(): String {
+        return "<li>${children.toHtml()}</li>"
+    }
+}
 
-data class StrapiRichBlockNode(
+data class StrapiRichHeadingNode(
     @JsonProperty("level")
     val level: Int?,
-    @JsonProperty("format")
-    val format: String?, // 'unordered' | 'ordered'
     @JsonProperty("children")
-    val children: List<StrapiRichText>, // TextNode | LinkNode | ListItemNode
-): StrapiRichText
+    val children: List<StrapiRichText>, // TextNode | LinkNode
+) : StrapiRichText {
+    private val logger: Logger = LoggerFactory.getLogger(StrapiRichHeadingNode::class.java)
+
+    override fun toHtml(): String {
+        return when (level) {
+            1 -> "<h1>${children.toHtml()}</h1>"
+            2 -> "<h2>${children.toHtml()}</h2>"
+            3 -> "<h3>${children.toHtml()}</h3>"
+            4 -> "<h4>${children.toHtml()}</h4>"
+            5 -> "<h5>${children.toHtml()}</h5>"
+            6 -> "<h6>${children.toHtml()}</h6>"
+            else -> {
+                logger.warn("Erreur dans la conversion Strapi des titres, level '$level' non reconnu")
+                return children.toHtml()
+            }
+        }
+    }
+}
+
+data class StrapiRichListNode(
+    @JsonProperty("format")
+    val format: String, // 'unordered' | 'ordered'
+    @JsonProperty("children")
+    val children: List<StrapiRichListItemNode>,
+) : StrapiRichText {
+    override fun toHtml(): String {
+        if (format == "ordered") {
+            return "<ol>${children.toHtml()}</ol>"
+        }
+
+        return "<ul>${children.toHtml()}</ul>"
+    }
+}
+
+data class StrapiRichParagraphNode(
+    @JsonProperty("children")
+    val children: List<StrapiRichText>, // TextNode | LinkNode
+) : StrapiRichText {
+    override fun toHtml(): String {
+        return "<p>${children.toHtml()}</p>"
+    }
+}
+
+data class StrapiRichQuoteNode(
+    @JsonProperty("children")
+    val children: List<StrapiRichText>, // TextNode | LinkNode
+) : StrapiRichText {
+    override fun toHtml(): String {
+        return "<blockquote>${children.toHtml()}</blockquote>"
+    }
+}
