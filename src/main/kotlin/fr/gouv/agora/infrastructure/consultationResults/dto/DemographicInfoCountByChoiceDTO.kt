33dfb2f0838e279e30a24a3dcdcc1ba224b74a10
Enzo Calvino <enzo.calvino@octo.com>
2024-07-01 15:58:15 +0200

    working

--- src/main/kotlin/fr/gouv/agora/infrastructure/consultationResults/dto/DemographicInfoCountByChoiceDTO.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/consultationResults/dto/DemographicInfoCountByChoiceDTO.kt
@@ -1,9 +1,7 @@
 package fr.gouv.agora.infrastructure.consultationResults.dto
 
-import org.springframework.data.rest.core.config.Projection
 import java.util.*
 
-@Projection(types = [DemographicInfoCountByChoiceDTO::class])
 interface DemographicInfoCountByChoiceDTO {
     val choiceId: UUID
     val key: String?
--- src/main/kotlin/fr/gouv/agora/infrastructure/consultationUpdates/dto/ConsultationUpdateHistoryWithDateDTO.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/consultationUpdates/dto/ConsultationUpdateHistoryWithDateDTO.kt
@@ -1,9 +1,7 @@
 package fr.gouv.agora.infrastructure.consultationUpdates.dto
 
-import org.springframework.data.rest.core.config.Projection
 import java.util.*
 
-@Projection(types = [ConsultationUpdateHistoryWithDateDTO::class])
 interface ConsultationUpdateHistoryWithDateDTO {
     val stepNumber: Int
     val type: String
