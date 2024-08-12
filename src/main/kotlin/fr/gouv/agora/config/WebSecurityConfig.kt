31d67801e2662b71cd6fadbf9404d60040464527
ATMA <pheno62@gmail.com>
2024-08-12 14:32:33 +0200

    fix : swagger cors

--- src/main/kotlin/fr/gouv/agora/config/WebSecurityConfig.kt
+++ src/main/kotlin/fr/gouv/agora/config/WebSecurityConfig.kt
@@ -6,10 +6,13 @@ import fr.gouv.agora.security.jwt.AuthenticationTokenFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpMethod.*
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.servlet.function.RouterFunctions.resources
 
 @Configuration
@@ -23,13 +26,19 @@ class WebSecurityConfig(private val authenticationTokenFilter: AuthenticationTok
     @Bean
     fun authenticatedFilterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf().disable()
-            .cors()
+            .cors().configurationSource {
+                val cors = CorsConfiguration();
+                cors.allowedMethods = arrayListOf(DELETE.name(), GET.name(), POST.name());
+                cors.applyPermitDefaultValues();
+                return@configurationSource cors
+            }
             .and()
             .exceptionHandling().authenticationEntryPoint(AgoraAuthenticationEntryPoint())
             .and()
             .securityMatcher("/**")
             .authorizeHttpRequests()
-            .requestMatchers("/swagger-config.json", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+            .requestMatchers("/swagger-config.json", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**")
+            .permitAll()
             .requestMatchers("/admin/**").hasAuthority(UserAuthorizationJWT.ADMIN_APIS.authority)
             .requestMatchers("/moderate/**").hasAuthority(UserAuthorizationJWT.MODERATE_QAG.authority)
             .requestMatchers("/signup", "/login").permitAll()
