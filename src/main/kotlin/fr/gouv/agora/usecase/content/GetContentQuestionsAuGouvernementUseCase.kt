68337c6b5bcc20aae19ce1676962fd251b118359
Antoine Mazure <pheno62@gmail.com>
2024-10-24 11:06:56 +0200

    feat : ajout texte nb questions sur Strapi (#568)

--- src/main/kotlin/fr/gouv/agora/usecase/content/GetContentQuestionsAuGouvernementUseCase.kt
+++ src/main/kotlin/fr/gouv/agora/usecase/content/GetContentQuestionsAuGouvernementUseCase.kt
@@ -1,13 +1,21 @@
 package fr.gouv.agora.usecase.content
 
+import fr.gouv.agora.domain.PageQuestionAuGouvernementContent
 import fr.gouv.agora.usecase.content.repository.ContentRepository
+import fr.gouv.agora.usecase.qag.repository.QagInfoRepository
 import org.springframework.stereotype.Service
 
 @Service
 class GetContentQuestionsAuGouvernementUseCase(
     private val contentRepository: ContentRepository,
+    private val qagInfoRepository: QagInfoRepository,
 ) {
-    fun execute(): String {
-        return contentRepository.getPageQuestionsAuGouvernement()
+    fun execute(): PageQuestionAuGouvernementContent {
+        val content = contentRepository.getPageQuestionsAuGouvernement()
+
+        val nombreDeQuestions = qagInfoRepository.getQagsCount(null)
+        val texteAvecNombreDeQuestions = content.texteTotalQuestions.replace("{}", nombreDeQuestions.toString())
+
+        return PageQuestionAuGouvernementContent(content.informationBottomsheet, texteAvecNombreDeQuestions)
     }
 }
