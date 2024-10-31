02ca45b05d6e7d37a6c2df61dc310aaef4ee4ea0
ATMA <pheno62@gmail.com>
2024-10-31 14:56:06 +0100

    a

--- src/main/kotlin/fr/gouv/agora/infrastructure/concertations/ConcertationController.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/concertations/ConcertationController.kt
@@ -3,10 +3,12 @@ package fr.gouv.agora.infrastructure.concertations
 import fr.gouv.agora.usecase.concertations.ConcertationJsonMapper
 import fr.gouv.agora.usecase.concertations.GetConcertationsUseCase
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/concertations")
@@ -20,6 +22,8 @@ class ConcertationController(
         val concertations = getConcertationsUseCase.execute()
         val concertationsJson = concertationJsonMapper.toConcertationJson(concertations)
 
-        return ResponseEntity.ok().body(concertationsJson)
+        return ResponseEntity.ok()
+            .cacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic())
+            .body(concertationsJson)
     }
 }
