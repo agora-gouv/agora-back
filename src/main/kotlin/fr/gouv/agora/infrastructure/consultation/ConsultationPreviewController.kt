182cbbbcce2fbb54128c08e9fe50d4d2ccca5bf8
ATMA <pheno62@gmail.com>
2024-10-31 14:48:15 +0100

    feat : cache dans les headers

--- src/main/kotlin/fr/gouv/agora/infrastructure/consultation/ConsultationPreviewController.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/consultation/ConsultationPreviewController.kt
@@ -4,9 +4,11 @@ import fr.gouv.agora.config.AuthentificationHelper
 import fr.gouv.agora.usecase.consultation.ConsultationPreviewUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.TimeUnit
 
 @RestController
 @Tag(name = "Consultations")
@@ -19,7 +21,9 @@ class ConsultationPreviewController(
     fun getConsultationPreviewOngoingList(): ResponseEntity<ConsultationPreviewJson> {
         val consultationPreviewPage = consultationPreviewUseCase.getConsultationPreviewPage()
 
-        return ResponseEntity.ok().body(
+        return ResponseEntity.ok()
+            .cacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic())
+            .body(
             consultationPreviewJsonMapper.toJson(
                 domainOngoingList = consultationPreviewPage.ongoingList,
                 domainFinishedList = consultationPreviewPage.finishedList,
