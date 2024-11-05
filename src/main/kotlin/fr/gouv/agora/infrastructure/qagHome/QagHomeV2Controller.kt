b4160a3155cde1cfbe4e000f17a912fd7b9dfc1d
Antoine Mazure <pheno62@gmail.com>
2024-11-05 14:09:16 +0100

    feat : cache dans les headers (#578)

--- src/main/kotlin/fr/gouv/agora/infrastructure/qagHome/QagHomeV2Controller.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/qagHome/QagHomeV2Controller.kt
@@ -7,10 +7,12 @@ import fr.gouv.agora.usecase.qag.GetQagErrorTextUseCase
 import fr.gouv.agora.usecase.qagPaginated.QagPaginatedV2UseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.TimeUnit
 
 @RestController
 @Tag(name = "QaG")
@@ -22,7 +24,6 @@ class QagHomeV2Controller(
     private val qagHomeJsonMapper: QagHomeJsonMapper,
     private val authentificationHelper: AuthentificationHelper,
 ) {
-
     @Operation(summary = "Get QaG Détails")
     @GetMapping("/v2/qags")
     fun getQagDetails(
@@ -34,39 +35,47 @@ class QagHomeV2Controller(
         val usedThematiqueId = thematiqueId.takeUnless { it.isNullOrBlank() }
         val usedFilterType = filterType.takeUnless { it.isBlank() }
 
-        return pageNumber.toIntOrNull()?.let { pageNumberInt ->
-            when (usedFilterType) {
-                "top" -> qagPaginatedV2UseCase.getPopularQagPaginated(
-                    userId = userId,
-                    pageNumber = pageNumberInt,
-                    thematiqueId = usedThematiqueId,
-                )
+        val pageNumberInt = pageNumber.toIntOrNull() ?: return ResponseEntity.badRequest().body(Unit)
 
-                "latest" -> qagPaginatedV2UseCase.getLatestQagPaginated(
-                    userId = userId,
-                    pageNumber = pageNumberInt,
-                    thematiqueId = usedThematiqueId,
-                )
+        val qagsAndMaxPageCount = when (usedFilterType) {
+            "top" -> qagPaginatedV2UseCase.getPopularQagPaginated(
+                userId = userId,
+                pageNumber = pageNumberInt,
+                thematiqueId = usedThematiqueId,
+            )
 
-                "supporting" -> qagPaginatedV2UseCase.getSupportedQagPaginated(
-                    userId = userId,
-                    pageNumber = pageNumberInt,
-                    thematiqueId = usedThematiqueId,
-                )
+            "latest" -> qagPaginatedV2UseCase.getLatestQagPaginated(
+                userId = userId,
+                pageNumber = pageNumberInt,
+                thematiqueId = usedThematiqueId,
+            )
 
-                "trending" -> qagPaginatedV2UseCase.getTrendingQag(userId = userId)
+            "supporting" -> qagPaginatedV2UseCase.getSupportedQagPaginated(
+                userId = userId,
+                pageNumber = pageNumberInt,
+                thematiqueId = usedThematiqueId,
+            )
 
-                else -> null
-            }?.let { qagsAndMaxPageCount ->
-                ResponseEntity.ok().body(qagPaginatedJsonMapper.toJson(qagsAndMaxPageCount))
-            } ?: ResponseEntity.badRequest().body(Unit)
-        } ?: ResponseEntity.badRequest().body(Unit)
+            "trending" -> qagPaginatedV2UseCase.getTrendingQag(userId = userId)
+
+            else -> null
+        }
+
+        if (qagsAndMaxPageCount == null) {
+            return ResponseEntity.badRequest().body(Unit)
+        }
+
+        return ResponseEntity.ok()
+            .cacheControl(CacheControl.maxAge(5, TimeUnit.MINUTES).cachePrivate())
+            .body(qagPaginatedJsonMapper.toJson(qagsAndMaxPageCount))
     }
 
     @Operation(summary = "Get QaG Status")
     @GetMapping("/qags/ask_status")
     fun askStatus(): ResponseEntity<*> {
-        return ResponseEntity.ok().body(
+        return ResponseEntity.ok()
+            .cacheControl(CacheControl.maxAge(5, TimeUnit.MINUTES).cachePrivate())
+            .body(
             qagHomeJsonMapper.toQagAskStatusJson(
                 getQagErrorTextUseCase.getGetQagErrorText(
                     userId = authentificationHelper.getUserId()!!
