6c5ce40292235f0e12cc27c0d21d31c51b3090d1
ATMA <pheno62@gmail.com>
2024-11-07 15:35:31 +0100

    suppression thematiques

--- src/main/kotlin/fr/gouv/agora/infrastructure/thematique/repository/ThematiqueMapper.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/thematique/repository/ThematiqueMapper.kt
@@ -4,29 +4,16 @@ import fr.gouv.agora.domain.Thematique
 import fr.gouv.agora.infrastructure.common.StrapiAttributes
 import fr.gouv.agora.infrastructure.common.StrapiDTO
 import fr.gouv.agora.infrastructure.common.StrapiData
+import fr.gouv.agora.infrastructure.thematique.dto.ThematiqueDatabaseDTO
 import fr.gouv.agora.infrastructure.thematique.dto.ThematiqueStrapiDTO
 import org.springframework.stereotype.Component
 
 @Component
 class ThematiqueMapper {
-<<<<<<< Updated upstream
-    fun toDomain(dto: ThematiqueStrapiDTO) = Thematique(
-        id = dto.databaseId,
-        label = dto.label,
-        picto = dto.pictogramme,
-=======
-
-    fun toDomain(dto: ThematiqueDatabaseDTO) = Thematique(
-        id = dto.id,
-        label = dto.label,
-        picto = dto.picto,
-    )
-
     fun toDomain(dto: StrapiAttributes<ThematiqueStrapiDTO>) = Thematique(
         id = dto.id,
         label = dto.attributes.label,
         picto = dto.attributes.pictogramme,
->>>>>>> Stashed changes
     )
 
     fun toDomain(strapiDTO: StrapiDTO<ThematiqueStrapiDTO>): List<Thematique> {
