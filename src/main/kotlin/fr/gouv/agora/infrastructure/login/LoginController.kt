85154baa342c87885fb82a968beed3f86e49b0fd
ATMA <pheno62@gmail.com>
2025-03-31 10:10:12 +0200

    feat : jwt cookie

--- src/main/kotlin/fr/gouv/agora/infrastructure/login/LoginController.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/login/LoginController.kt
@@ -3,14 +3,21 @@ package fr.gouv.agora.infrastructure.login
 import fr.gouv.agora.domain.AgoraFeature
 import fr.gouv.agora.domain.LoginRequest
 import fr.gouv.agora.infrastructure.utils.IpAddressUtils
+import fr.gouv.agora.security.jwt.AuthenticationTokenFilter.Companion.JWT_COOKIE_KEY
+import fr.gouv.agora.security.jwt.JwtTokenUtils
+import fr.gouv.agora.security.jwt.JwtTokenUtils.JWT_TOKEN_VALIDITY
 import fr.gouv.agora.usecase.appVersionControl.AppVersionControlUseCase
-import fr.gouv.agora.usecase.appVersionControl.AppVersionStatus.*
+import fr.gouv.agora.usecase.appVersionControl.AppVersionStatus.AUTHORIZED
+import fr.gouv.agora.usecase.appVersionControl.AppVersionStatus.INVALID_APP
+import fr.gouv.agora.usecase.appVersionControl.AppVersionStatus.UPDATE_REQUIRED
 import fr.gouv.agora.usecase.featureFlags.FeatureFlagsUseCase
 import fr.gouv.agora.usecase.login.LoginUseCase
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseCookie
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -23,7 +30,6 @@ class LoginController(
     private val appVersionControlUseCase: AppVersionControlUseCase,
     private val loginTokenGenerator: LoginTokenGenerator,
     private val loginUseCase: LoginUseCase,
-    private val loginInfoJsonMapper: LoginInfoJsonMapper,
     private val featureFlagsUseCase: FeatureFlagsUseCase,
 ) {
 
@@ -41,28 +47,56 @@ class LoginController(
             return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
         }
 
-        return when (appVersionControlUseCase.getAppVersionStatus(platform = platform, versionCode = versionCode)) {
-            INVALID_APP -> ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
-            UPDATE_REQUIRED -> ResponseEntity.status(HttpServletResponse.SC_PRECONDITION_FAILED)
-                .body(Unit)
+        val appVersionStatus =
+            appVersionControlUseCase.getAppVersionStatus(platform = platform, versionCode = versionCode)
 
-            AUTHORIZED -> when (val loginTokenResult = loginTokenGenerator.decodeLoginToken(loginRequest.loginToken)) {
-                DecodeResult.Failure -> ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
-                is DecodeResult.Success -> loginUseCase.login(
-                    loginRequest = LoginRequest(
-                        userId = loginTokenResult.loginTokenData.userId,
-                        ipAddressHash = IpAddressUtils.retrieveIpAddressHash(request),
-                        userAgent = userAgent,
-                        fcmToken = fcmToken ?: "",
-                        platform = platform,
-                        versionName = versionName ?: "($versionCode)",
-                        versionCode = versionCode,
-                    )
-                )?.let { userInfo ->
-                    ResponseEntity.ok().body(loginInfoJsonMapper.toJson(domain = userInfo))
-                } ?: ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
-            }
+        if (appVersionStatus == INVALID_APP) {
+            return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
         }
-    }
+        if (appVersionStatus == UPDATE_REQUIRED) {
+            return ResponseEntity.status(HttpServletResponse.SC_PRECONDITION_FAILED).body(Unit)
+        }
+        if (appVersionStatus != AUTHORIZED) {
+            return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
+        }
 
+        val loginTokenResult = loginTokenGenerator.decodeLoginToken(loginRequest.loginToken)
+        if (loginTokenResult is DecodeResult.Failure) {
+            return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
+        }
+
+        val userInfo = loginUseCase.login(
+            loginRequest = LoginRequest(
+                userId = (loginTokenResult as DecodeResult.Success).loginTokenData.userId,
+                ipAddressHash = IpAddressUtils.retrieveIpAddressHash(request),
+                userAgent = userAgent,
+                fcmToken = fcmToken ?: "",
+                platform = platform,
+                versionName = versionName ?: "($versionCode)",
+                versionCode = versionCode,
+            )
+        )
+
+        if (userInfo == null) {
+            return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(Unit)
+        }
+
+        val (jwtToken, expirationEpochMilli) = JwtTokenUtils.generateToken(userId = userInfo.userId)
+        val jwtCookie = ResponseCookie.from(JWT_COOKIE_KEY, jwtToken)
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(JWT_TOKEN_VALIDITY)
+            .build()
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+            .body(
+                LoginInfoJson(
+                    jwtToken = jwtToken,
+                    jwtExpirationEpochMilli = expirationEpochMilli,
+                    isModerator = false,
+                )
+            )
+    }
 }
--- src/main/kotlin/fr/gouv/agora/security/jwt/JwtTokenUtils.kt
+++ src/main/kotlin/fr/gouv/agora/security/jwt/JwtTokenUtils.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object JwtTokenUtils {
 
-    private val JWT_TOKEN_VALIDITY = TimeUnit.DAYS.toMillis(1)
+    val JWT_TOKEN_VALIDITY = TimeUnit.DAYS.toMillis(1)
     private const val JWT_PREFIX = "Bearer "
 
     fun generateToken(userId: String, claims: Map<String, Any> = emptyMap()): Pair<String, Long> {
