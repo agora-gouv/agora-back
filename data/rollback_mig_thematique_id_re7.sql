64aee3a30b648fa4035567427c5b7b6e9ed7b70b
@wojciech.wojcik <wojciech.wojcik@gmail.com>
2026-06-03 18:11:07 +0200

    sql de rollback pour les migration de thematique id

---
+++ data/rollback_mig_thematique_id_re7.sql
@@ -0,0 +1,26 @@
+UPDATE qags SET thematique_id = '1' WHERE thematique_id = 'vzc2huds18u09xwvhc2s5eif';
+UPDATE qags SET thematique_id = '2' WHERE thematique_id = 'xtcppdt11wtn5vq616ke5k5o';
+UPDATE qags SET thematique_id = '3' WHERE thematique_id = 'boo0psoizocgdy2t5jnntpt3';
+UPDATE qags SET thematique_id = '4' WHERE thematique_id = 'j8df5ma2y6hmphlczm3onqka';
+UPDATE qags SET thematique_id = '5' WHERE thematique_id = 'jdanft4g347wifqqwsppdpy0';
+UPDATE qags SET thematique_id = '6' WHERE thematique_id = 'swuwv9agz5aq8w038numftuk';
+UPDATE qags SET thematique_id = '7' WHERE thematique_id = 'vpnxcj3cd10q6psm3alfvs48';
+UPDATE qags SET thematique_id = '8' WHERE thematique_id = 'mljxtr9g6iapig426eq5znyn';
+UPDATE qags SET thematique_id = '9' WHERE thematique_id = 'imeyo2dypj208p2zlmcraz61';
+UPDATE qags SET thematique_id = '10' WHERE thematique_id = 'jipt6yrf7059bdg4qjhcsvaq';
+
+UPDATE qags SET thematique_id = '11' WHERE thematique_id = 'vl5o51mivbj2c669nipc15d1';
+UPDATE qags SET thematique_id = '12' WHERE thematique_id = 's2yx3unrkhqmwi4piw7o6t5y';
+UPDATE qags SET thematique_id = '13' WHERE thematique_id = 'gvigqh4k9wtorn6biggf0xbm';
+UPDATE qags SET thematique_id = '14' WHERE thematique_id = 'fb3hm6ob0062qqj6vo2wgaw2';
+UPDATE qags SET thematique_id = '15' WHERE thematique_id = 'nchjcdzi38jn9qsd7l7wa3et';
+UPDATE qags SET thematique_id = '16' WHERE thematique_id = 'ua2s5vxdb66ocvri8u03xr6n';
+UPDATE qags SET thematique_id = '17' WHERE thematique_id = 'y1m3xao2suvwq0cax5opyaen';
+UPDATE qags SET thematique_id = '18' WHERE thematique_id = 'ejlsbawqoppnkkishp2lyo1z';
+UPDATE qags SET thematique_id = '19' WHERE thematique_id = 'kcd7w5wgjxbhrgx8ix3av59f';
+UPDATE qags SET thematique_id = '20' WHERE thematique_id = 'ew95vhtcag0oodilg4nythd1';
+
+UPDATE qags SET thematique_id = '21' WHERE thematique_id = 'oeb1e26n34se1y356dhixa4w';
+UPDATE qags SET thematique_id = '22' WHERE thematique_id = 'nq8lfxdv112810dozl8253lv';
+UPDATE qags SET thematique_id = '23' WHERE thematique_id = 'p4hjthihpnhc4p3ge70oqgtz';
+UPDATE qags SET thematique_id = '24' WHERE thematique_id = 'pkp9jhfjbwkxqrosldx0w1n6';
