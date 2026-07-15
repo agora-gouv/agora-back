1bb9bf74189cf00122007f371c4b52df97457390
@wojciech.wojcik <wojciech.wojcik@gmail.com>
2026-07-15 17:22:26 +0200

    fix route challenge ACME

--- src/main/kotlin/fr/gouv/agora/infrastructure/acme/AcmeController.kt
+++ src/main/kotlin/fr/gouv/agora/infrastructure/acme/AcmeController.kt
@@ -1,7 +1,6 @@
 package fr.gouv.agora.infrastructure.acme
 
 import fr.gouv.agora.usecase.acme.repository.AcmeChallengeStore
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,10 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AcmeController(private val challengeStore: AcmeChallengeStore) {
 
-    @GetMapping(
-        "/.well-known/acme-challenge/{token}",
-        produces = [MediaType.TEXT_PLAIN_VALUE],
-    )
+    @GetMapping("/.well-known/acme-challenge/{token}")
     fun getChallenge(@PathVariable token: String): ResponseEntity<String> {
         val keyAuthorization = challengeStore.getChallenge(token)
             ?: return ResponseEntity.notFound().build()
